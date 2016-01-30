acdbf01106116e6e97fbecba3ce61daf85ead8eb
Deni Hamdani <pii.deni.prg@gmail.com>
2016-01-30 10:27:33 +0900

    Filter pencarian pasien rujukan radiologi

--- protected/modules/radiologi/controllers/RujukanPenunjangController.php
+++ protected/modules/radiologi/controllers/RujukanPenunjangController.php
@@ -4,14 +4,26 @@ class RujukanPenunjangController extends MyAuthController
 {
 	public function actionIndex()
 	{
+            $model = new PasienkirimkeunitlainV;
+            $model->tgl_awal = date('Y-m-d', strtotime('-5 days'));
+            $model->tgl_akhir = date('Y-m-d');
+            
+            if (isset($_GET['PasienkirimkeunitlainV'])) {
+                $model->attributes = $_GET['PasienkirimkeunitlainV'];
+                $model->tgl_awal = MyFormatter::formatDateTimeForDb($_GET['PasienkirimkeunitlainV']['tgl_awal']);
+                $model->tgl_akhir = MyFormatter::formatDateTimeForDb($_GET['PasienkirimkeunitlainV']['tgl_akhir']);
+            }
+            
+            $dataProvider = $model->searchRujukRad();
+            /*
 			$criteria = new CDbCriteria;
 			if(isset($_GET['ajax']) && $_GET['ajax']=='pasienpenunjangrujukan-m-grid') {
 				$format = new MyFormatter;
-				echo $format->formatDateTimeForDb($_GET['tgl_akhir']);
-				$criteria->compare('LOWER(t.no_pendaftaran)', strtolower($_GET['noPendaftaran']),true);
-				$criteria->compare('LOWER(t.nama_pasien)', strtolower($_GET['namaPasien']),true);
-				$criteria->compare('LOWER(t.no_rekam_medik)', strtolower($_GET['noRekamMedik']),true);
-				$criteria->addBetweenCondition('t.tgl_kirimpasien::date', $format->formatDateTimeForDb($_GET['tgl_awal']), $format->formatDateTimeForDb($_GET['tgl_akhir']));
+				//echo $format->formatDateTimeForDb($_GET['tgl_akhir']);
+				if (isset($_GET['no_pendaftaran'])) $criteria->compare('LOWER(t.no_pendaftaran)', strtolower($_GET['noPendaftaran']),true);
+				if (isset($_GET['nama_pasien'])) $criteria->compare('LOWER(t.nama_pasien)', strtolower($_GET['namaPasien']),true);
+				if (isset($_GET['noRekamMedik'])) $criteria->compare('LOWER(t.no_rekam_medik)', strtolower($_GET['noRekamMedik']),true);
+				if (isset($_GET['tgl_awal']) && isset($_GET['tgl_akhir'])) $criteria->addBetweenCondition('t.tgl_kirimpasien::date', $format->formatDateTimeForDb($_GET['tgl_awal']), $format->formatDateTimeForDb($_GET['tgl_akhir']));
 			} else {
 //                $criteria->addBetweenCondition('tgl_pendaftaran', date('Y-m-d'), date('Y-m-d'));
 				$criteria->addBetweenCondition('date(t.tgl_pendaftaran)', date('Y-m-d', strtotime('-5 days')).' 00:00:00', date('Y-m-d H:i:s'));
@@ -25,7 +37,9 @@ class RujukanPenunjangController extends MyAuthController
 			$dataProvider = new CActiveDataProvider(PasienkirimkeunitlainV::model(), array(
 			'criteria'=>$criteria,
 		));
-			$this->render('index',array('dataProvider'=>$dataProvider));
+             * 
+             */
+			$this->render('index',array('dataProvider'=>$dataProvider, 'model'=>$model));
 	}
 
 	// Uncomment the following methods and override them if needed
