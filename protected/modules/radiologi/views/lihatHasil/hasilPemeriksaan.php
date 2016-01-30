93ad26bc0f1256049914b4221e3cc32128264586
Deni Hamdani <pii.deni.prg@gmail.com>
2016-01-30 12:07:51 +0900

    Printout Hasil Pemeriksaan RAD

--- protected/modules/radiologi/views/lihatHasil/hasilPemeriksaan.php
+++ protected/modules/radiologi/views/lihatHasil/hasilPemeriksaan.php
@@ -154,7 +154,7 @@
     <table width="100%" border="0" cellpadding="0" cellspacing="0">
         <tr>
             <td align="left" width="50%">&nbsp;</td>
-            <td align="center">PEMERIKSA</td>
+            <td align="center">Salam Sejawat,</td>
         </tr>
         <tr>
             <td align="left">
