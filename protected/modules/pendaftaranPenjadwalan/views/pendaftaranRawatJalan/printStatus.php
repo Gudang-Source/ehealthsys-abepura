c20a787e9057d7e4bf9ecbac9174c329834fde6d
Deni Hamdani <pii.deni.prg@gmail.com>
2016-02-03 12:06:39 +0900

    Print Status Pendaftaran

--- protected/modules/pendaftaranPenjadwalan/views/pendaftaranRawatJalan/printStatus.php
+++ protected/modules/pendaftaranPenjadwalan/views/pendaftaranRawatJalan/printStatus.php
@@ -8,52 +8,51 @@
     td, th{
         font-size: 11pt !important;
     }
-    body{
-        width: 21.7cm;
-    }
 </style>
 <?php echo $this->renderPartial('pendaftaranPenjadwalan.views.pendaftaranRawatJalan._headerPrintStatus'); ?>
-
 <table width="100%">
     <tr>
-        <td colspan="">No. Rekam Medik &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <?php echo $modPasien->no_rekam_medik; ?></td>
-        <td colspan=""></td>
-        <td colspan="3">&nbsp;</td>
-        <td colspan="">Tanggal Pengisian</td>
-        <td colspan=""><?php echo MyFormatter::formatDateTimeId($modPendaftaran->tgl_pendaftaran); ?></td>
+        <td nowrap>No. Rekam Medik</td>
+        <td width="100%">: <?php echo $modPasien->no_rekam_medik; ?></td>
+        <td nowrap>Tanggal Pengisian</td>
+        <td width="50%" nowrap>: <?php echo MyFormatter::formatDateTimeId($modPendaftaran->tgl_pendaftaran); ?></td>
+    </tr>
+</table>
+<table width="100%">
+    
+    <tr>
+        <td align="center" valig="middle" colspan="4" style="text-decoration: underline; font-weight: bold;"><br/>DATA UMUM PASIEN</td>
     </tr>
     <tr>
-        <td align="center" valig="middle" colspan="9">DATA UMUM PASIEN</td>
+        <td valig="middle" colspan="4" style="font-weight:bold">Data Awal Pasien</td>
     </tr>
     <tr>
-        <td valig="middle" colspan="9" style="font-weight:bold">Data Awal Pasien</td>
+        <td>Nama lengkap pasien <!--sesuai KTP/Identitas lain--></td>
+        <td><?php echo $modPasien->namadepan.$modPasien->nama_pasien.(!empty($modPasien->nama_bin) ? " (".$modPasien->nama_bin.")" : ""); ?></td>
     </tr>
     <tr>
-        <td colspan="5">Nama lengkap pasien sesuai KTP/Identitas lain</td>
-        <td colspan="4"><?php echo $modPasien->namadepan.$modPasien->nama_pasien.(!empty($modPasien->nama_bin) ? " (".$modPasien->nama_bin.")" : ""); ?></td>
+        <td>Nama panggilan</td>
+        <td><?php echo isset($modPasien->nama_bin)?$modPasien->nama_bin:"-"; ?></td>
     </tr>
     <tr>
-        <td colspan="5">Nama panggilan</td>
-        <td colspan="4"><?php echo isset($modPasien->nama_bin)?$modPasien->nama_bin:"-"; ?></td>
+        <td>Alamat lengkap</td>
+        <td><?php echo isset($modPasien->alamat_pasien)?$modPasien->alamat_pasien:"-"; ?></td>
     </tr>
     <tr>
-        <td colspan="5">Alamat lengkap</td>
-        <td colspan="4"><?php echo isset($modPasien->alamat_pasien)?$modPasien->alamat_pasien:"-"; ?></td>
+        <td>Kota/Kabupaten</td>
+        <td><?php echo $modPasien->kabupaten->kabupaten_nama; ?></td>
     </tr>
     <tr>
-        <td colspan="5"></td>
         <td>Kecamatan</td>
         <td><?php echo isset($modPasien->kecamatan->kecamatan_nama)?$modPasien->kecamatan->kecamatan_nama:"-"; ?></td>
+    </tr>
+    <tr>
         <td>Kelurahan</td>
         <td><?php echo isset($modPasien->kelurahan->kelurahan_nama)?$modPasien->kelurahan->kelurahan_nama:"-"; ?></td>
     </tr>
     <tr>
-        <td colspan="5"></td>
-        <td>Kota</td>
-        <td><?php echo $modPasien->kabupaten->kabupaten_nama; ?></td>
-        <td>Kode pos</td>
-        <td><?php echo isset($modPasien->kelurahan->kode_pos)?$modPasien->kelurahan->kode_pos:"-"; ?></td>
+        <td>Kode Pos</td>
+        <td><?php echo !empty($modPasien->kelurahan->kode_pos)?$modPasien->kelurahan->kode_pos:"-"; ?></td>
     </tr>
 <!--     <tr>
         <td colspan="5"></td>
@@ -63,77 +62,79 @@
         <td></td>
     </tr> -->
     <tr>
-        <td colspan="5">Nomor Telepon</td>
-        <td>Rumah</td>
-        <td><?php echo isset($modPasien->no_telepon_pasien)?$modPasien->no_telepon_pasien:' - '; ?></td>
-        <td>No. Mobile</td>
-        <td><?php echo isset($modPasien->no_mobile_pasien)?$modPasien->no_mobile_pasien:' - '; ?></td>
+        <td>Nomor Telepon</td>
+        <td><?php echo !empty($modPasien->no_telepon_pasien)?$modPasien->no_telepon_pasien:' - '; ?></td>
     </tr>
     <tr>
-        <td colspan="5">Tempat Tanggal Lahir</td>
-        <td colspan="4"><?php echo (isset($modPasien->tempat_lahir)?$modPasien->tempat_lahir:' - ').", ".(isset($modPasien->tanggal_lahir)?MyFormatter::formatDateTimeId($modPasien->tanggal_lahir):' - '); ?></td>
+        <td>Nomor Mobile</td>
+        <td><?php echo !empty($modPasien->no_mobile_pasien)?$modPasien->no_mobile_pasien:' - '; ?></td>
     </tr>
     <tr>
-        <td colspan="5">Jenis Kelamin</td>
-        <td colspan="4"><?php echo isset($modPasien->jeniskelamin)?$modPasien->jeniskelamin:' - '; ?></td>
+        <td>Tempat Tanggal Lahir</td>
+        <td><?php echo (!empty($modPasien->tempat_lahir)?$modPasien->tempat_lahir:' - ').", ".(isset($modPasien->tanggal_lahir)?MyFormatter::formatDateTimeId($modPasien->tanggal_lahir):' - '); ?></td>
     </tr>
     <tr>
-        <td colspan="5">Status Perkawinan</td>
-        <td colspan="4"><?php echo isset($modPasien->statusperkawinan)?$modPasien->statusperkawinan:' - '; ?></td>
+        <td>Jenis Kelamin</td>
+        <td><?php echo !empty($modPasien->jeniskelamin)?$modPasien->jeniskelamin:' - '; ?></td>
     </tr>
     <tr>
-        <td colspan="5">Kewarganegaraan</td>
-        <td colspan="4"><?php echo isset($modPasien->warga_negara)?$modPasien->warga_negara:' - '; ?></td>
+        <td>Status Perkawinan</td>
+        <td><?php echo !empty($modPasien->statusperkawinan)?$modPasien->statusperkawinan:' - '; ?></td>
     </tr>
     <tr>
-        <td colspan="5">Agama</td>
-        <td colspan="4"><?php echo isset($modPasien->agama)?$modPasien->agama:' - '; ?></td>
+        <td>Kewarganegaraan</td>
+        <td><?php echo !empty($modPasien->warga_negara)?$modPasien->warga_negara:' - '; ?></td>
     </tr>
     <tr>
-        <td colspan="5">Pekerjaan</td>
-        <td colspan="4"><?php echo isset($modPasien->pekerjaan_id)?$modPasien->pekerjaan->pekerjaan_nama:' - '; ?></td>
+        <td>Agama</td>
+        <td><?php echo !empty($modPasien->agama)?$modPasien->agama:' - '; ?></td>
     </tr>
     <tr>
-        <td colspan="5">Pendidikan</td>
-        <td colspan="4"><?php echo isset($modPasien->pendidikan_id)?$modPasien->pendidikan->pendidikan_nama:' - '; ?></td>
+        <td>Pekerjaan</td>
+        <td><?php echo !empty($modPasien->pekerjaan_id)?$modPasien->pekerjaan->pekerjaan_nama:' - '; ?></td>
     </tr>
     <tr>
-        <td colspan="5">Email</td>
-        <td colspan="4"><?php echo isset($modPasien->alamatemail)?$modPasien->alamatemail:' - '; ?></td>
+        <td>Pendidikan</td>
+        <td><?php echo !empty($modPasien->pendidikan_id)?$modPasien->pendidikan->pendidikan_nama:' - '; ?></td>
+    </tr>
+    <tr>
+        <td>Email</td>
+        <td><?php echo !empty($modPasien->alamatemail)?$modPasien->alamatemail:' - '; ?></td>
     </tr>
      <tr>
-        <td valig="middle" colspan="9" style="font-weight:bold">Dalam keadaan darurat dapat menghubungi</td>
+         <td valig="middle" colspan="4" style="font-weight:bold"><br/>Dalam keadaan darurat dapat menghubungi</td>
     </tr>
     <tr>
-        <td colspan="5">Nama</td>
-        <td colspan="4"><?php echo isset($modPenanggungjawab->nama_pj)?$modPenanggungjawab->nama_pj:" - "; ?></td>
+        <td>Nama</td>
+        <td><?php echo !empty($modPenanggungjawab->nama_pj)?$modPenanggungjawab->nama_pj:" - "; ?></td>
     </tr>
     <tr>
-        <td colspan="5">Hubungan dengan pasien</td>
-        <td colspan="4"><?php echo isset($modPenanggungjawab->hubungankeluarga)?$modPenanggungjawab->hubungankeluarga:" - "; ?></td>
+        <td>Hubungan dengan pasien</td>
+        <td><?php echo !empty($modPenanggungjawab->hubungankeluarga)?$modPenanggungjawab->hubungankeluarga:" - "; ?></td>
     </tr>
     <tr>
-        <td colspan="5">Alamat lengkap</td>
-        <td colspan="4"><?php echo isset($modPenanggungjawab->alamat_pj)?$modPenanggungjawab->alamat_pj:' - '; ?></td>
+        <td>Alamat lengkap</td>
+        <td><?php echo !empty($modPenanggungjawab->alamat_pj)?$modPenanggungjawab->alamat_pj:' - '; ?></td>
     </tr>
     <tr>
-        <td colspan="5">Nomor Telepon</td>
-        <td>Rumah</td>
-        <td><?php echo isset($modPenanggungjawab->no_teleponpj)?$modPenanggungjawab->no_teleponpj:' - '; ?></td>
+        <td>Nomor Telepon</td>
+        <td><?php echo !empty($modPenanggungjawab->no_teleponpj)?$modPenanggungjawab->no_teleponpj:' - '; ?></td>
+    </tr>
+    <tr>
         <td>No. Mobile</td>
-        <td><?php echo isset($modPenanggungjawab->no_mobilepj)?$modPenanggungjawab->no_mobilepj:' - '; ?></td>
+        <td><?php echo !empty($modPenanggungjawab->no_mobilepj)?$modPenanggungjawab->no_mobilepj:' - '; ?></td>
     </tr>
 <!--     <tr>
         <td colspan="5">Tempat Tanggal Lahir</td>
-        <td colspan="4"><?php echo (isset($modPasien->tempat_lahir)?$modPasien->tempat_lahir:' - ').", ".(isset($modPasien->tanggal_lahir)?MyFormatter::formatDateTimeId($modPasien->tanggal_lahir):' - '); ?></td>
+        <td colspan="4"><?php echo (!empty($modPasien->tempat_lahir)?$modPasien->tempat_lahir:' - ').", ".(isset($modPasien->tanggal_lahir)?MyFormatter::formatDateTimeId($modPasien->tanggal_lahir):' - '); ?></td>
     </tr>
     <tr>
         <td colspan="5">Tempat Tanggal Lahir</td>
-        <td colspan="4"><?php echo (isset($modPasien->tempat_lahir)?$modPasien->tempat_lahir:' - ').", ".(isset($modPasien->tanggal_lahir)?MyFormatter::formatDateTimeId($modPasien->tanggal_lahir):' - '); ?></td>
+        <td colspan="4"><?php echo (!empty($modPasien->tempat_lahir)?$modPasien->tempat_lahir:' - ').", ".(isset($modPasien->tanggal_lahir)?MyFormatter::formatDateTimeId($modPasien->tanggal_lahir):' - '); ?></td>
     </tr>
     <tr>
         <td colspan="5">Tempat Tanggal Lahir</td>
-        <td colspan="4"><?php echo (isset($modPasien->tempat_lahir)?$modPasien->tempat_lahir:' - ').", ".(isset($modPasien->tanggal_lahir)?MyFormatter::formatDateTimeId($modPasien->tanggal_lahir):' - '); ?></td>
+        <td colspan="4"><?php echo (!empty($modPasien->tempat_lahir)?$modPasien->tempat_lahir:' - ').", ".(isset($modPasien->tanggal_lahir)?MyFormatter::formatDateTimeId($modPasien->tanggal_lahir):' - '); ?></td>
     </tr> -->
 </table>
 
