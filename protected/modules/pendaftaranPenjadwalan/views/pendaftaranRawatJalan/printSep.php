64cc366e7833dce4607d9d021cd65e9ce63b49c9
Iqbal Laksana <iqbal_laksana01@yahoo.co.id>
2016-02-25 19:45:15 +0900

    ABE-441: print surat elegibilitas peserta

--- protected/modules/pendaftaranPenjadwalan/views/pendaftaranRawatJalan/printSep.php
+++ protected/modules/pendaftaranPenjadwalan/views/pendaftaranRawatJalan/printSep.php
@@ -1,42 +1,164 @@
 <?php $data=ProfilrumahsakitM::model()->findByPk(Params::DEFAULT_PROFIL_RUMAH_SAKIT); ?>
-<style>
+<style>   
     .barcode-label{
         margin-top:-20px;
         z-index: 1;
         text-align: center;
         letter-spacing: 10px;
     }
-    td, th{
+    td{
         font-size: 11pt !important;
     }
     body{
         width: 21.7cm;
     }
+    
+    td.header
+    {
+        padding-left:30px;
+    }
 </style>
 <?php //echo $this->renderPartial('pendaftaranPenjadwalan.views.pendaftaranRawatJalan._headerPrintStatus'); ?>
+<table width="100%" border = "0" style = "text-align:left;">
+    <thead>
+        <th width = "25%" style = "padding:20px;"><img src="<?php echo Yii::app()->getBaseUrl('webroot').'/images/BPJS.jpg'; ?>" width="300px" style = "height:50px;"></th>
+        <th align='center' style="font-weight:bold;padding:0px;"><font style="font-size:15px;"><?php echo $judul_print; ?><br><?php echo $data->nama_rumahsakit; ?></font></th>        
+        <th  style = "padding:0px;"><img src="<?php echo Params::urlProfilRSDirectory().$data->logo_rumahsakit ?>" width="120px"></th>
+    </thead>
+    <tbody>
+        <td colspan = "4">
+            <table border = "0" width=100%' style = "text-align:left;">
+                <tr>
+                    <td class = "header">No. SEP</td>
+                    <td>:</td>
+                    <td><?php echo $modSep->nosep; ?></td>
+                    <td>&nbsp;</td>
+                    <td>No. DMK</td>
+                    <td>:</td>
+                    <td><?php echo $modPasien->no_rekam_medik; ?></td>
+                </tr>
+                <tr>
+                    <td class = "header">Tgl. SEP</td>
+                    <td>:</td>
+                    <td><?php echo $modSep->tglsep; ?></td>
+                    <td>&nbsp;</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td class = "header">No. Kartu</td>
+                    <td>:</td>
+                    <td><?php echo $modSep->nokartuasuransi; ?></td>
+                    <td>&nbsp;</td>
+                    <td>Peserta</td>
+                    <td>:</td>
+                    <td><?php echo isset($modJenisPeserta->jenispeserta_nama)?$modJenisPeserta->jenispeserta_nama:'-';?></td>
+                </tr>
+                <tr>
+                    <td class = "header">Nama Peserta</td>
+                    <td>:</td>
+                    <td><?php echo $modAsuransiPasienBpjs->namapemilikasuransi;?></td>
+                    <td>&nbsp;</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td class = "header">Tgl. Lahir</td>
+                    <td>:</td>
+                    <td><?php echo $modPasien->tanggal_lahir; ?></td>
+                    <td>&nbsp;</td>
+                    <td>COB</td>
+                    <td>:</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td class = "header">Jenis Kelamin</td>
+                    <td>:</td>
+                    <td><?php echo $modPasien->jeniskelamin; ?></td>
+                    <td>&nbsp;</td>
+                    <td>Jenis Rawat</td>
+                    <td>:</td>
+                    <td><?php echo LookupM::model()->findByAttributes(array('lookup_type'=>'jenispelayanan','lookup_value'=>$modSep->jnspelayanan))->lookup_name; ?></td>
+                </tr>
+                <tr>
+                    <td class = "header">Poli Tujuan</td>
+                    <td>:</td>
+                    <td><?php echo $modPendaftaran->ruangan->ruangan_nama; ?></td>
+                    <td>&nbsp;</td>
+                    <td>Kelas Rawat</td>
+                    <td>:</td>
+                    <td><?php echo $modAsuransiPasienBpjs->kelastanggunganasuransi->kelasbpjs_id; ?></td>
+                </tr>
+                <tr>
+                    <td class = "header">Asal Faskes Tk. I</td>
+                    <td>:</td>
+                    <td><?php echo "-"; ?></td>
+                    <td>&nbsp;</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td class = "header">Diagnosa Awal</td>
+                    <td>:</td>
+                    <td><?php echo $modSep->diagnosaawal; ?></td>
+                    <td>&nbsp;</td>
+                    <td>Pasien/<br>Keluarga Pasien</td>
+                    <td style = "padding-left:20px">&nbsp;</td>
+                    <td>Petugas<br>BPJS Kesehatan</td>
+                </tr>
+                <tr>
+                    <td class = "header">Catatan</td>
+                    <td>:</td>
+                    <td><?php echo $modSep->catatansep; ?></td>
+                    <td>&nbsp;</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    
+                </tr>
+                <tr>
+                    <td class = "header" colspan = "3" style="font-size: 8pt !important;">*Saya Menyetujui BPJS Kesehatan menggunakan informasi Medis Pasien jika diperlukan<br>*SEP bukan sebagai bukti penjaminan peserta</td>
+                    <td>&nbsp;</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>                    
+                </tr>
+                <tr>
+                    <td class = "header" colspan = "3" style="font-size: 8pt !important;"></td>
+                    <td>&nbsp;</td>
+                    <td><div style = "padding:0px;margin:0px;height:10px;width:120px;border-top: solid 1px #000;"></div></td>
+                    <td></td>
+                    <td><div style = "padding:0px;margin:0px;height:10px;width:120px;border-top: solid 1px #000;"></div></td>                    
+                </tr>
+            </table>
+        </td>
+    </tbody>
+</table>
 
-<table width="100%">
+<!--
+<p>&nbsp;</p>
+<table width="100%" border = "1">
     <tr>
-        <td rowspan='2'><img src="<?php echo Yii::app()->getBaseUrl('webroot').'/images/BPJS.jpg'; ?>" width="200"></td>
-        <td colspan='5' align='center' style="font-weight:bold"><?php echo $judul_print; ?><br><?php echo $data->nama_rumahsakit; ?></td>
-        <td rowspan='2'><img src="<?php echo Params::urlProfilRSDirectory().$data->logo_rumahsakit ?>" width="120"></td>
-    </tr>
-    <tr>
-        <td colspan='5' align='center' style="font-weight:bold"></td>
-    </tr>
-    <tr>
-        <td width="18%">No. SEP</td>
+        <td colspan ="2"  style = "padding:0px;"><img src="<?php// echo Yii::app()->getBaseUrl('webroot').'/images/BPJS.jpg'; ?>" width="300px" style = "height:50px;"></td>
+        <td colspan = "4" align='center' style="font-weight:bold;padding:0px;"><?php //echo $judul_print; ?><br><?php echo $data->nama_rumahsakit; ?></td>        
+        <td  style = "padding:0px;"><img src="<?php //echo Params::urlProfilRSDirectory().$data->logo_rumahsakit ?>" width="120px"></td>
+    </tr>    
+    <tr>        
+        <td width="1%" >No. SEP</td>
         <td width="2%">:</td>
-        <td width="25%"><?php echo $modSep->nosep; ?></td>
+        <td width="25%"><?php //echo $modSep->nosep; ?></td>
         <td width="5%"></td>
         <td width="18%">No. D.M.K.</td>
         <td width="2%">:</td>
-        <td width="30%"><?php echo $modPasien->no_rekam_medik; ?></td>
+        <td width="30%"><?php //echo $modPasien->no_rekam_medik; ?></td>
     </tr>
     <tr>
         <td>Tgl. SEP</td>
         <td>:</td>
-        <td><?php echo $modSep->tglsep; ?></td>
+        <td><?php //echo $modSep->tglsep; ?></td>
         <td></td>
         <td></td>
         <td></td>
@@ -45,16 +167,16 @@
     <tr>
         <td>No. Kartu</td>
         <td>:</td>
-        <td><?php echo $modSep->nokartuasuransi; ?></td>
+        <td><?php //echo $modSep->nokartuasuransi; ?></td>
         <td></td>
         <td>Peserta</td>
         <td>:</td>
-        <td><?php echo isset($modJenisPeserta->jenispeserta_nama)?$modJenisPeserta->jenispeserta_nama:'-';?></td>
+        <td><?php //echo isset($modJenisPeserta->jenispeserta_nama)?$modJenisPeserta->jenispeserta_nama:'-';?></td>
     </tr>
     <tr>
         <td>Nama Peserta</td>
         <td>:</td>
-        <td><?php echo $modAsuransiPasienBpjs->namapemilikasuransi;?></td>
+        <td><?php //echo $modAsuransiPasienBpjs->namapemilikasuransi;?></td>
         <td></td>
         <td></td>
         <td></td>
@@ -63,7 +185,7 @@
     <tr>
         <td>Tgl. Lahir</td>
         <td>:</td>
-        <td><?php echo $modPasien->tanggal_lahir; ?></td>
+        <td><?php //echo $modPasien->tanggal_lahir; ?></td>
         <td></td>
         <td>COB</td>
         <td>:</td>
@@ -72,25 +194,25 @@
     <tr>
         <td>Jenis Kelamin</td>
         <td>:</td>
-        <td><?php echo $modPasien->jeniskelamin; ?></td>
+        <td><?php //echo $modPasien->jeniskelamin; ?></td>
         <td></td>
         <td>Jenis Rawat</td>
         <td>:</td>
-        <td><?php echo LookupM::model()->findByAttributes(array('lookup_type'=>'jenispelayanan','lookup_value'=>$modSep->jnspelayanan))->lookup_name; ?></td>
+        <td><?php //echo LookupM::model()->findByAttributes(array('lookup_type'=>'jenispelayanan','lookup_value'=>$modSep->jnspelayanan))->lookup_name; ?></td>
     </tr>
     <tr>
         <td>Poli Tujuan</td>
         <td>:</td>
-        <td><?php echo $modPendaftaran->ruangan->ruangan_nama; ?></td>
+        <td><?php// echo $modPendaftaran->ruangan->ruangan_nama; ?></td>
         <td></td>
         <td>Kelas Rawat</td>
         <td>:</td>
-        <td><?php echo $modAsuransiPasienBpjs->kelastanggunganasuransi->kelasbpjs_id; ?></td>
+        <td><?php //echo $modAsuransiPasienBpjs->kelastanggunganasuransi->kelasbpjs_id; ?></td>
     </tr>
     <tr>
         <td width="18%">Asal Faskes Tk.I</td>
         <td width="2%">:</td>
-        <td width="25%"><?php echo "-"; ?></td>
+        <td width="25%"><?php //echo "-"; ?></td>
         <td width="5%"></td>
         <td width="18%"></td>
         <td width="2%"></td>
@@ -99,7 +221,7 @@
     <tr>
         <td>Diagnosa Awal</td>
         <td>:</td>
-        <td><?php echo $modSep->diagnosaawal; ?></td>
+        <td><?php //echo $modSep->diagnosaawal; ?></td>
         <td></td>
         <td>Pasien/<br>Keluarga Pasien</td>
         <td></td>
@@ -108,7 +230,7 @@
     <tr>
         <td>Catatan</td>
         <td>:</td>
-        <td><?php echo $modSep->catatansep; ?></td>
+        <td><?php //echo $modSep->catatansep; ?></td>
         <td></td>
         <td></td>
         <td></td>
@@ -119,10 +241,10 @@
         <td></td>
         <td colspan="3">______________________________________</td>
     </tr>
-    <tr>
+   <tr>
         <td colspan="3"></td>
         <td colspan="3"></td>
-        <td ><img src="<?php echo Params::urlProfilRSDirectory().$data->logo_rumahsakit ?>" width="120"></td>
+        <td ><img src="<?php //echo Params::urlProfilRSDirectory().$data->logo_rumahsakit ?>" width="120"></td>
     </tr>
 
-</table>
+</table>-->
