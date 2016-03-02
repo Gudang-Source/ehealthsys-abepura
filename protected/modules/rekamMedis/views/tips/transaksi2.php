332a9794abb0c804ed89c15c11e284b6643a80a3
Iqbal Laksana <iqbal_laksana01@yahoo.co.id>
2016-03-02 17:27:26 +0900

    ABE-484: checklist dan fix modul rekam medis (menu master data pasien)

---
+++ protected/modules/rekamMedis/views/tips/transaksi2.php
@@ -0,0 +1,41 @@
+<span class="required"><i>Bagian dengan tanda * harus diisi.</i></span>
+<p>
+<table border="0" style="padding :none;">
+  <tr>
+    <td>1. </td>
+    <td>Icon <i class="icon-calendar"></i> untuk menentukan tanggal</td>
+  </tr>
+  <tr>
+    <td>2. </td>
+    <td>Icon <i class="icon-refresh"></i> untuk mengembalikan data kesemula.</td>
+  </tr>
+    <tr>
+      <td>3. </td>
+        <td>Gunakan icon ini <i class="icon-list"></i> berfungsi untuk menampilkan lis</td>
+  </tr>.
+    <tr>    
+       <td>4. </td>
+        <td>Gunakan tombol ini  <button class="btn btn-primary" name="yt0" onkeypress="return formSubmit(this,event)" type="submit">
+<i class="icon-ok icon-white"></i>
+Simpan
+</button>
+Cari
+</button> berfungsi untuk menyimpan</td>
+  <tr>
+    <td>5. </td>
+    <td>Gunakan tombol ini <a class="btn btn-danger" href="/simrs/index.php?r=rawatInap/pasienRawatInap/formTindakLanjutDariPasienRI">
+<i class="icon-refresh icon-white"></i>
+Ulang
+</a> untunk mengulang kembali inputan.</td>
+  </tr>
+   </tr>
+     <tr>
+	 <td>7. </td>
+        <td>Gunakan tombol ini <a class="btn btn-info" href="#">
+<i class="icon-print icon-white"></i>
+Print Kartu Pasien
+</a> berfungsi untuk mencetak data kartu pasien.</td>
+ </tr>
+</table>
+</p>
+
