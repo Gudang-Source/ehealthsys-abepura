d465d1a00466af00dad3b66f2c93973f20c702f5
Iqbal Laksana <iqbal_laksana01@yahoo.co.id>
2016-03-07 16:50:26 +0900

    ABE-534

---
+++ protected/modules/sistemAdministrator/views/detailPemeriksaanLab/tips/tipsCreate.php
@@ -0,0 +1,31 @@
+<span class="required"><i>Bagian dengan tanda * harus diisi.</i></span>
+<p>
+<table border="0" style="padding :none;">
+<!--  <tr>
+    <td>1. </td>
+    <td>Icon <i class="icon-remove"></i> untuk membatalkan data.</td>
+  </tr>-->
+   <tr>
+    <td>1. </td>
+    <td>Tombol <button class="btn btn-primary" name="yt1" onclick="" type="button">
+<i class="icon-ok icon-white"></i>
+Simpan
+</button> untuk menyimpan data.</td>
+  </tr>
+  <tr>
+    <td>2. </td>
+    <td>Tombol <button class="btn btn-danger" name="yt1" onclick="" type="button">
+<i class="icon-refresh icon-white"></i>
+Ulang
+</button> untuk mengulang halaman.</td>
+  </tr>
+ 
+  <tr>
+    <td>3. </td>
+    <td>Tombol <a class="btn btn-success" href="#">
+<i class="icon-file icon-white"></i>
+Pengaturan 
+</a> untuk kembali ke pengaturan data.</td>
+  </tr>
+</table>
+</p>
