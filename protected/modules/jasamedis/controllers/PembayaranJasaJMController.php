535ba78c1bf3af8ad4e3a63e5a9fd30c9d1d41c0
Deni Hamdani <pii.deni.prg@gmail.com>
2016-03-07 15:03:38 +0900

    ABE-301 Tambah Modul Jasa Medis

---
+++ protected/modules/jasamedis/controllers/PembayaranJasaJMController.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of PembayaranjasaJMController
+ *
+ * Extend dari modul Penggajian
+ * 
+ * @author Deni Hamdani
+ * @email <pii.deni.prg@gmail.org>
+ */
+Yii::import("penggajian.controllers.PembayaranJasaController");
+Yii::import("penggajian.views.pembayaranJasa");
+class PembayaranJasaJMController extends PembayaranJasaController {
+    //put your code here
+}
