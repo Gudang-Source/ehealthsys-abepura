be8dbf9f4f08aa4c9af1e7866e31f2dfc752b887
Iqbal Laksana <iqbal_laksana01@yahoo.co.id>
2016-03-11 13:31:18 +0900

    ABE-508: Checklist dan Fix modul gudangFarmasi (Master menu Obat SUplier dan Obat Alkes)

--- protected/modules/sistemAdministrator/views/obatSupplier/_form.php
+++ protected/modules/sistemAdministrator/views/obatSupplier/_form.php
@@ -95,13 +95,14 @@ $this->widget('application.extensions.moneymask.MMask', array(
                         <?php echo CHtml::link(Yii::t('mds','{icon} Ulang',array('{icon}'=>'<i class="icon-refresh icon-white"></i>')), 
                                     Yii::app()->createUrl($this->module->id.'/ObatSupplier/admin'), 
                                     array('class'=>'btn btn-danger',
-                                          'onclick'=>'myConfirm("Apakah anda ingin mengulang ini?","Perhatian!",function(r){if(r) window.location = window.location.href;}); return false;'));  ?>
+                                          'onclick'=>'myConfirm("Apakah anda ingin mengulang ini?","Perhatian!",function(r){if(r) window.location = window.location.href;}); return false;'));  ?>                        
+                        <?php echo CHtml::link(Yii::t('mds', '{icon} Pengaturan Obat Supplier', array('{icon}'=>'<i class="icon-folder-open icon-white"></i>')),
+                                $this->createUrl(Yii::app()->controller->id.'/admin',array('modul_id'=> Yii::app()->session['modul_id'])), array('class'=>'btn btn-success'));?>
+                
                         <?php
                             $content = $this->renderPartial($this->path_tips.'tips.tipsaddedit3',array(),true);
                             $this->widget('UserTips',array('type'=>'transaksi','content'=>$content));
                         ?>
-                        <?php echo CHtml::link(Yii::t('mds', '{icon} Pengaturan Obat Supplier', array('{icon}'=>'<i class="icon-folder-open icon-white"></i>')),
-                                $this->createUrl(Yii::app()->controller->id.'/admin',array('modul_id'=> Yii::app()->session['modul_id'])), array('class'=>'btn btn-success'));?>
             </div>
 
 <?php $this->endWidget(); ?>
