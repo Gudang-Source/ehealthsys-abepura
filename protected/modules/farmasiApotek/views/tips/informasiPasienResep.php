cc0ebde395545f816532062cc6ae23b07614756b
Iqbal Laksana <iqbal_laksana01@yahoo.co.id>
2016-03-23 14:22:19 +0900

    ABE-655: Fail Checklist Pada Menu : Informasi > Pasien Resep

---
+++ protected/modules/farmasiApotek/views/tips/informasiPasienResep.php
@@ -0,0 +1,34 @@
+<span class="required"><i>Bagian dengan tanda * harus diisi.</i></span>
+<p>
+<table border="0" style="padding :none;">
+  <tr>
+    <td>1. </td>
+    <td>Icon <i class="icon-calendar"></i>untuk menentukan tanggal Pendaftaran.</td>
+  </tr>
+  <tr>
+    <td>2. </td>
+    <td>Icon <i class="icon-form-reseptur"></i>untuk print reseptur.</td>
+  </tr>
+  <tr>
+    <td>3. </td>
+    <td>Icon <i class="icon-form-jualresep"></i>untuk melanjutkan ke penjualan resep.</td>
+  </tr>
+  <tr>
+    <td>4. </td>
+    <td>Icon <i class="icon-form-copy"></i>untuk membuat salinan resep.</td>
+  </tr>
+  <tr>
+    <td>5. </td>
+    <td>Gunakan tombol ini  <a class="btn btn-primary" name="yt0"><i class="icon-search icon-white"></i>
+Cari
+</a> berfungsi untuk mencari data</td>
+  </tr>
+  <tr>
+    <td>6. </td>
+    <td>Gunakan tombol ini <a class="btn btn-danger">
+<i class="icon-refresh icon-white"></i>
+Ulang
+</a> untunk mengulang kembali inputan.</td>
+  </tr>
+</table>
+</p>
