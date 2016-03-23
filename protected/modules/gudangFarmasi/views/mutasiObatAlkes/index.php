acbcbf6b280a3a863da70ac5469584c291e5c15b
Iqbal Laksana <iqbal_laksana01@yahoo.co.id>
2016-03-23 17:46:05 +0900

    ABE-675: Simulasi > Rawat darurat > Menu transaksi > Mutasi Obat Alkes : Tombol Ulang refres sendiri. Isi Petunjuk Nomor 1-5 tidak ada dalam apliksasi

--- protected/modules/gudangFarmasi/views/mutasiObatAlkes/index.php
+++ protected/modules/gudangFarmasi/views/mutasiObatAlkes/index.php
@@ -95,8 +95,9 @@
                 ?>
                 <?php echo CHtml::link(Yii::t('mds','{icon} Ulang',array('{icon}'=>'<i class="icon-refresh icon-white"></i>')), 
                         $this->createUrl($this->id.'/index'), 
-                        array('class'=>'btn btn-danger',
-                              'onclick'=>'refreshForm(this);')); ?>
+                                array('class'=>'btn btn-danger',
+//                                      'onclick'=>'if(!confirm("Apakah anda ingin mengulang ini ?")) return false;'));
+                                      'onclick'=>'myConfirm("Apakah anda ingin mengulang ini?","Perhatian!",function(r){if(r) window.location = "'.$this->createUrl($this->id.'/index').'";}); return false;')); ?>
                 <?php 
                 $content = $this->renderPartial($this->path_view.'tips/tipsMutasiObatAlkes',array(),true);
                 $this->widget('UserTips',array('type'=>'transaksi','content'=>$content));  
--- protected/modules/gudangFarmasi/views/mutasiObatAlkes/tips/tipsMutasiObatAlkes.php
+++ protected/modules/gudangFarmasi/views/mutasiObatAlkes/tips/tipsMutasiObatAlkes.php
@@ -1,19 +1,11 @@
+Bagian dengan tanda bintang <span class="required">*</span> harus diisi.
 <ol class="row-fluid">
-    <div class="span6">
+    <div class="span6">        
         <li>
-            Bagian dengan tanda bintang <span class="required">*</span> harus diisi.
-        </li>
+            Icon <i class="icon-calendar"></i>untuk menentukan tanggal.
+        </li>               
         <li>
-            Icon <i class="icon-calendar"></i><i class="icon-time"></i> untuk menentukan tanggal dan waktu pendaftaran.
-        </li>
-        <li>
-            Icon <i class="icon-chevron-down"></i> untuk menampilkan form yang tersembunyi.
-        </li>
-        <li>
-            Icon <i class="icon-list-alt"></i> berfungsi untuk mencari dan menampilkan daftar data sesuai yang diketikan.
-        </li>
-        <li>
-            Icon <span class="add-on"><a id="" href="javascript:void(0);"><i class="icon-list-alt"></i><i class="icon-search"></i></a></span> 
+            Icon <span class="add-on"><a id="" href="javascript:void(0);"><i class="icon-list"></i><i class="icon-search"></i></a></span> 
             berfungsi untuk mencari dan menampilkan daftar data sesuai yang diketikan serta menampilkan dialog box jika diklik.
         </li>
 <!--        <li>
@@ -25,15 +17,15 @@
             berfungsi untuk menambah data.
         </li>
         <li>
-            Tombol  <div class="btn btn-danger" name="yt0" type="button"><i class="icon-refresh icon-white"></i></div>
-            berfungsi untuk me-refresh form.
-        </li>
-    </div>
-    <div class="span6">
-        <li>
             Tombol  <div class="btn btn-primary" name="yt0" type="button"><i class="icon-ok icon-white"></i> Simpan</div>
             berfungsi untuk menyimpan.
         </li>
+       <!-- <li>
+            Tombol  <div class="btn btn-danger" name="yt0" type="button"><i class="icon-refresh icon-white"></i></div>
+            berfungsi untuk me-refresh form.
+        </li>-->
+    </div>
+    <div class="span6">
         <li>
             Tombol  <div class="btn btn-danger" name="yt0" type="button"><i class="icon-refresh icon-white"></i> Ulang</div>
             berfungsi untuk mengulang transaksi.
