5963da2b7e558e7f9a19d402531d36696edcc309
Iqbal Laksana <iqbal_laksana01@yahoo.co.id>
2016-03-28 15:33:08 +0900

    ABE-709 Fail Checklist Pada Menu : Informasi > Daftar PAsien

---
+++ protected/modules/radiologi/views/tips/informasiDaftarPasien.php
@@ -0,0 +1,54 @@
+<span class="required"><i>Bagian dengan tanda * harus diisi.</i></span>
+<p>
+<table border="0" style="padding :none;">
+  <tr>
+    <td style = "vertical-align:middle;">1. </td>
+    <td>Icon <i class="icon-calendar"></i><i class="icon-time"></i> untuk menentukan tanggal dan waktu pendaftaran.</td>
+    <td>&nbsp;</td>
+    <td style = "vertical-align:middle;">7. </td>
+    <td>Icon <i class="icon-list-alt"></i> berfungsi untuk menampilkan list data secara detail.</td>
+  </tr>
+  <tr>
+    <td style = "vertical-align:middle;">2. </td>
+    <td>Icon <button class = "btn btn-primary"><i class="icon-volume-up icon-white"></i></button> untuk memanggil pasien.
+    </td>
+    <td>&nbsp;</td>
+    <td style = "vertical-align:middle;">8. </td>
+    <td>Gunakan tombol ini  <button class="btn btn-primary" name="yt0" type="submit"><i class="icon-search icon-white"></i>
+Cari
+</button> berfungsi untuk mencari data.</td>
+  </tr>
+  <tr>
+    <td style = "vertical-align:middle;">3. </td>
+    <td>Icon <i class="icon-form-ubah"></i> untuk mengubah pemeriksaan.</td>
+    <td>&nbsp;</td>
+    <!--<td style = "vertical-align:middle;">9. </td>
+    <td>Gunakan tombol ini  <a id="selectPasien" class="btn btn-success" href="/simrs/index.php?r=radiologi/masukPenunjang&idPasienKirimKeUnitLain=203&pendaftaran_id=104" target="blank" rel="tooltip" data-original-title="Klik untuk periksa pasien">Periksa</a> berfungsi untuk memeriksa data pasien.</td>-->
+    <td style = "vertical-align:middle;">9. </td>
+    <td>Gunakan tombol ini <a class="btn btn-danger">
+<i class="icon-refresh icon-white"></i>
+Ulang
+</a> untuk mengulang kembali inputan.</td>
+  </tr>
+  <tr>
+    <td style = "vertical-align:middle;">4. </td>
+    <td>Icon <i class="icon-form-input"></i> untuk memasukkan hasil.</td>
+    <td>&nbsp;</td>
+    <td style = "vertical-align:middle;">10. </td>
+    <td>Gunakan tombol ini <a class="btn btn-info">
+<i class="icon-print icon-white"></i>
+Print
+</a> berfungsi untuk mencetak data.</td>
+  </tr>
+  <tr>
+    <td style = "vertical-align:middle;">5. </td>
+    <td>Icon <i class="icon-form-lihat"></i> untuk melihat hasil pemeriksaan.</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td style = "vertical-align:middle;">6. </td>
+    <td>Icon <i class="icon-form-silang"></i> untuk membatalkan pemeriksaan.</td>
+    <td>&nbsp;</td>
+  </tr>  
+</table>
+</p>
