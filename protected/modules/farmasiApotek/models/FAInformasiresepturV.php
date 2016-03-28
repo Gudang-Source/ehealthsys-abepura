526e8abf100baf5fcd2164307c444a2d1570fa7f
Deni Hamdani <pii.deni.prg@gmail.com>
2016-03-28 15:44:19 +0900

    ABE-706 Filter ruangan informasi pasien resep

--- protected/modules/farmasiApotek/models/FAInformasiresepturV.php
+++ protected/modules/farmasiApotek/models/FAInformasiresepturV.php
@@ -12,6 +12,13 @@ class FAInformasiresepturV extends InformasiresepturV
 	public function searchInformasiPasienResep()
     {
         $criteria=$this->criteriaSearch();
+        if (empty($this->ruanganreseptur_id)) {
+            if (Yii::app()->user->getState('ruangan_nama') == "Apotek Rawat Jalan"):
+                $criteria->addInCondition('instalasireseptur_id', array(2));
+            else:
+                $criteria->addInCondition('instalasireseptur_id', array(3,4));
+            endif;
+        }
         $criteria->addBetweenCondition('date(t.tglreseptur)', $this->tgl_awal, $this->tgl_akhir);
         $criteria->order = 't.tglreseptur DESC';
         $criteria->join = "join pendaftaran_t p on p.pendaftaran_id = t.pendaftaran_id";
