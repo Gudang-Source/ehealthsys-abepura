e94c1e0de0a73e0cfee2423f1b82d7a03339db36
Iqbal Laksana <iqbal_laksana01@yahoo.co.id>
2016-03-28 10:47:34 +0900

    ABE-705: Tidak ada dalam Petunjuk

---
+++ protected/modules/persalinan/views/informasiTarif/tips/informasiTarifPersalinan.php
@@ -0,0 +1,19 @@
+<span class="required"><i>Bagian dengan tanda * harus diisi.</i></span>
+<p>
+<table border="0" style="padding :none;">  
+    <tr>
+        <td style = "vertical-align:middle;">1. </td>
+        <td>Icon ini  
+            <i class="icon-form-komtarif"></i> untuk melihat detail tarif. </td>
+    </tr>
+    <tr>
+        <td  style = "vertical-align:middle;">1. </td>
+        <td>Gunakan tombol ini  <a class="btn btn-primary" name="yt0">
+            <i class="icon-search icon-white"></i>Cari</a> berfungsi untuk mencari data. </td>
+    </tr>
+    <tr>
+        <td  style = "vertical-align:middle;">2. </td>
+        <td>Gunakan tombol ini <a class="btn btn-danger"><i class="icon-refresh icon-white"></i>Ulang</a> untunk mengulang kembali inputan.</td>
+    </tr>
+</table>
+</p>
