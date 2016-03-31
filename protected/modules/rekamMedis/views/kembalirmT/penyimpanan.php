b34d70a6859062099bd37a733d1c5fb0acc766ac
Iqbal Laksana <iqbal_laksana01@yahoo.co.id>
2016-03-31 15:37:13 +0900

    ABE-730: Fail Checklist Pada Menu : Transaksi > Penyimpanan Dokumen Rekam Medis Baru - ada yg ketinggalan tadi

--- protected/modules/rekamMedis/views/kembalirmT/penyimpanan.php
+++ protected/modules/rekamMedis/views/kembalirmT/penyimpanan.php
@@ -222,7 +222,7 @@
                 array('class'=>'btn btn-danger',
                     'onclick'=>'myConfirm("Apakah anda ingin mengulang ini?","Perhatian!",function(r) {if(r) window.location = "'.$this->createUrl('Penyimpanan').'";} ); return false;'));  ?>
             <?php 
-            $content = $this->renderPartial('../tips/transaksi',array(),true);
+            $content = $this->renderPartial('../tips/transaksiPenyimpananDokumenRM',array(),true);
             $this->widget('UserTips',array('type'=>'transaksi','content'=>$content));  ?>	
 
         </div>
