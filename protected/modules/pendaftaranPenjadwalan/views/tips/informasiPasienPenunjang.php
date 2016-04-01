8b2492e6c8f1e6d67018680870498ded830d3c4c
Iqbal Laksana <iqbal_laksana01@yahoo.co.id>
2016-04-01 14:22:49 +0900

    ABE-748: Fail Checklist Pada Menu : Informasi > Pasien Penunjang

---
+++ protected/modules/pendaftaranPenjadwalan/views/tips/informasiPasienPenunjang.php
@@ -0,0 +1,32 @@
+<span class="required"><i>Bagian dengan tanda * harus diisi.</i></span>
+<p>
+<table border="0" style="padding :none;">
+  <tr>
+    <td style="vertical-align: middle;">1. </td>
+    <td>Icon <i class="icon-calendar"></i> untuk menentukan tanggal.</td>
+  </tr>  
+  <tr>
+    <td style="vertical-align: middle;">2. </td>
+    <td>Icon <i class="icon-form-ubah"></i> untuk mengedit lembaran kerja terpilih.</td>
+  </tr>  
+  <tr>
+    <td style="vertical-align: middle;">3. </td>
+    <td>Gunakan tombol ini  <button class="btn btn-primary" type="button"><i class="icon-search icon-white"></i>
+Cari
+</button> berfungsi untuk mencari data.</td>
+  </tr>
+  <tr>
+    <td style="vertical-align: middle;">4. </td>
+    <td>Gunakan tombol ini <a class="btn btn-danger"">
+<i class="icon-refresh icon-white"></i>
+Ulang
+</a> untunk mengulang kembali inputan.</td>
+  </tr>
+    <tr>
+    <td style="vertical-align: middle;">5. </td>
+    <td>Gunakan tombol ini  <button class="btn btn-primary" type="button"><i class="icon-ok icon-white"></i>
+Simpan
+</button> berfungsi untuk menyimpan data.</td>
+  </tr>
+</table>
+</p>
