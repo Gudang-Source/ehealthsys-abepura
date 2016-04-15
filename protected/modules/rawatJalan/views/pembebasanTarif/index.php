d89e183c3f60190ce5ebbe7d09da9fac2b8bcb8a
Iqbal Laksana <iqbal_laksana01@yahoo.co.id>
2016-04-15 16:43:50 +0900

    ABE-890: Fail Checklist Pada Menu : Transaksi > Pembebasan Tarif

--- protected/modules/rawatJalan/views/pembebasanTarif/index.php
+++ protected/modules/rawatJalan/views/pembebasanTarif/index.php
@@ -117,7 +117,7 @@
                         array('class'=>'btn btn-danger',
                               'onclick'=>'myConfirm("Apakah anda ingin mengulang ini?","Perhatian!",function(r){if(r) window.location = window.location.href;}); return false;')); */ ?>
             <?php
-    $content = $this->renderPartial('../tips/transaksi',array(),true);
+    $content = $this->renderPartial('../tips/transaksiPembebasanTarif',array(),true);
     $this->widget('UserTips',array('type'=>'transaksi','content'=>$content)); 
     ?>
         </div>
