89b15b9dc52b6408e679497f71ff16f32a95c132
Iqbal Laksana <iqbal_laksana01@yahoo.co.id>
2016-04-15 15:51:49 +0900

    ABE-915: Fail Checklist Pada Menu : Informasi > Tarif Rawat Jalan

--- protected/modules/rawatJalan/views/informasiTarif/index.php
+++ protected/modules/rawatJalan/views/informasiTarif/index.php
@@ -80,7 +80,7 @@
                                                         array('class'=>'btn btn-danger',
                                                                   'onclick'=>'myConfirm("Apakah anda ingin mengulang ini?","Perhatian!",function(r){if(r) window.location = window.location.href;}); return false;'));  ?>
                 <?php 
-                   $content = $this->renderPartial('rawatJalan.views.tips.informasi',array(),true);
+                   $content = $this->renderPartial('rawatJalan.views.tips.informasiTarif',array(),true);
                         $this->widget('UserTips',array('type'=>'admin','content'=>$content));
                 ?>
         </div>
