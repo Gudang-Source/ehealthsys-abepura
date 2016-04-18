d0509c3b426a26e7032268f428b4a69a4458f2d4
Iqbal Laksana <iqbal_laksana01@yahoo.co.id>
2016-04-18 13:20:55 +0900

    ABE-855: Fail Checklist Pada Menu : Informasi > Pemesanan Kamar

--- protected/modules/pendaftaranPenjadwalan/views/tips/informasiBookingKamar.php
+++ protected/modules/pendaftaranPenjadwalan/views/tips/informasiBookingKamar.php
@@ -17,14 +17,18 @@
     <td style="vertical-align: middle;">4. </td>
     <td>Icon <i class="icon-form-sampah"></i> untuk membatalkan.</td>
   </tr>  
-  <tr>     
+  <tr>
     <td style="vertical-align: middle;">5. </td>
+    <td>Icon <i class="icon-form-ri"></i> untuk mendaftarkan pasien ke rawat inap.</td>
+  </tr>
+  <tr>
+    <td style="vertical-align: middle;">6. </td>
     <td>Gunakan tombol ini  <button class="btn btn-primary" type="button"><i class="icon-search icon-white"></i>
 Cari
 </button> berfungsi untuk mencari data.</td>
   </tr>
   <tr>
-    <td style="vertical-align: middle;">6. </td>
+    <td style="vertical-align: middle;">7. </td>
     <td>Gunakan tombol ini <a class="btn btn-danger">
 <i class="icon-refresh icon-white"></i>
 Ulang
