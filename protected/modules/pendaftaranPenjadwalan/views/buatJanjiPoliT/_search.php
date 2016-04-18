f3cec760e58f8692f7bbd2fcdfdec0c64adc486f
Iqbal Laksana <iqbal_laksana01@yahoo.co.id>
2016-04-18 13:08:56 +0900

    ABE-854: Fail Checklist Pada Menu : Informasi > Pembuatan Janji Polik

--- protected/modules/pendaftaranPenjadwalan/views/buatJanjiPoliT/_search.php
+++ protected/modules/pendaftaranPenjadwalan/views/buatJanjiPoliT/_search.php
@@ -66,7 +66,7 @@
                                 array('class'=>'btn btn-danger',
                                     'onclick'=>'return refreshForm(this);'));  ?>
     <?php  
-        $content = $this->renderPartial('pendaftaranPenjadwalan.views.tips.informasi',array(),true);
+        $content = $this->renderPartial('pendaftaranPenjadwalan.views.tips.informasiBuatJanjiPoli',array(),true);
         $this->widget('UserTips',array('type'=>'transaksi','content'=>$content)); ?>	
 	
 </div>
