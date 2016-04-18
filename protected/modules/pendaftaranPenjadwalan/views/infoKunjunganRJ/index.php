504562f6cb8ee6e76dcbf11907b4269075c5f09a
Iqbal Laksana <iqbal_laksana01@yahoo.co.id>
2016-04-18 11:48:17 +0900

    ABE-847: Fail Checklist Pada Menu : Informasi > Rawat Jalan

--- protected/modules/pendaftaranPenjadwalan/views/infoKunjunganRJ/index.php
+++ protected/modules/pendaftaranPenjadwalan/views/infoKunjunganRJ/index.php
@@ -402,7 +402,7 @@ $('.search-form form').submit(function(){
                                     array('class'=>'btn btn-danger',
                                         'onclick'=>'myConfirm("Apakah anda ingin mengulang ini?","Perhatian!",function(r) {if(r) window.location = "'.$this->createUrl('index').'";} ); return false;'));  ?>
         <?php 
-        $content = $this->renderPartial('pendaftaranPenjadwalan.views.tips.informasi',array(),true);
+        $content = $this->renderPartial('pendaftaranPenjadwalan.views.tips.informasiPasienRJ',array(),true);
         $this->widget('UserTips',array('type'=>'transaksi','content'=>$content)); ?>	
 
     </div>
