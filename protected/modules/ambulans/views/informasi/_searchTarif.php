9df3cec6673705e37b8b584147b1405168b7f896
Iqbal Laksana <iqbal_laksana01@yahoo.co.id>
2016-04-19 09:38:29 +0900

    ABE-933: fail checklist pada menu : informasi > tarif ambulans

--- protected/modules/ambulans/views/informasi/_searchTarif.php
+++ protected/modules/ambulans/views/informasi/_searchTarif.php
@@ -87,7 +87,7 @@
                         array('class'=>'btn btn-danger',
                               'onclick'=>'myConfirm("Apakah anda ingin mengulang ini?","Perhatian!",function(r){if(r) window.location = window.location.href;}); return false;'));  ?>								
             <?php
-            $content = $this->renderPartial('../tips/informasi',array(),true);
+            $content = $this->renderPartial('../tips/informasiTarifAmbulans',array(),true);
             $this->widget('UserTips',array('type'=>'transaksi','content'=>$content)); 
         ?>
     </div>
