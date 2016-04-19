d355aeddae0cd694bdc12bca63f1770c985819fc
Iqbal Laksana <iqbal_laksana01@yahoo.co.id>
2016-04-19 11:17:43 +0900

    ABE-926

---
+++ protected/modules/ambulans/views/tips/transaksiPemakaianAmbulansPRS.php
@@ -0,0 +1,81 @@
+<span class="required"><i>Bagian dengan tanda * harus diisi.</i></span>
+<p>
+<table border="0" style="padding :none;">
+    <tr>
+        <td style = "vertical-align: middle;">1. </td>
+        <td>Icon <i class="icon-calendar"></i><i class="icon-time"></i> berfungsi untuk menentukan tanggal dan waktu.</td>
+    </tr>   
+    <tr>
+        <td style = "vertical-align: middle;">2. </td>
+        <td>Icon <i class="icon-list"></i> berfungsi untuk menampilkan list data sesuai dengan yang diketikkan.</td>
+    </tr>
+    <tr>
+        <td style = "vertical-align: middle;">3. </td>
+        <td>Icon <i class="icon-list"></i><i class="icon-search"></i> berfungsi untuk menampilkan list data sesuai dengan yang diketikkan dan icon di klik menampilkan dialog box pencarian.</td>
+    </tr>           
+    <tr>
+        <td style = "vertical-align: middle;">4. </td>
+        <td>Icon <i class="icon-form-sampah"></i> berfungsi untuk menghapus data.</td>
+    </tr> 
+    <tr>
+        <td style = "vertical-align: middle;">5. </td>
+        <td>Icon <i class="icon-form-silang"></i> berfungsi untuk membatalkan data.</td>
+    </tr> 
+    <tr>  
+        <td style = "vertical-align: middle;">6. </td>
+        <td>Gunakan tombol ini <button type="button" class = "btn btn-primary">
+                <i class="icon-plus-sign icon-white"></i>
+                </button> berfungsi untuk menambahkan data
+        </td>
+    </tr>
+    <tr>  
+        <td style = "vertical-align: middle;">7. </td>
+        <td>Gunakan tombol ini <button type="button" class = "btn btn-primary">
+                <i class="icon-search icon-white"></i>
+                </button> berfungsi untuk mencari data, dengan menampilkan dialog box.
+        </td>
+    </tr>
+    <tr>  
+        <td style = "vertical-align: middle;">8. </td>
+        <td>Gunakan tombol ini  <button class="btn btn-primary" type="button">
+            <i class="icon-ok icon-white"></i>
+            Simpan
+            </button>
+            berfungsi untuk menyimpan</td>
+    </tr> 
+    <tr>
+       <td style = "vertical-align: middle;">9. </td>
+       <td>Gunakan tombol ini <a class="btn btn-danger" >
+            <i class="icon-refresh icon-white"></i>
+            Ulang
+            </a> berfungsi untuk mengulang kembali inputan.</td>
+     </tr>
+     <tr>
+       <td style = "vertical-align: middle;">10. </td>
+       <td>Tombol ini <a class="btn btn-info disabled" >
+            <i class="icon-print icon-white"></i>            
+            </a> berarti tidak dapat digunakan (tidak aktif), sedangkan 
+            Tombol ini <a class="btn btn-info" >
+            <i class="icon-print icon-white"></i>            
+            </a> berarti dapat digunakan (aktif).
+       </td>
+     </tr>
+     <tr>
+       <td style = "vertical-align: middle;">11. </td>
+       <td>Gunakan Tombol ini <a class="btn btn-info" >
+            <i class="icon-print icon-white"></i> 
+            Print Status            
+            </a> berfungsi untuk mencetak status ambulans.
+       </td>
+     </tr>
+     <tr>
+       <td style = "vertical-align: middle;">12. </td>
+       <td>Gunakan Tombol ini <a class="btn btn-info" >
+            <i class="icon-print icon-white"></i> 
+            Print Pemakaian Bmhp            
+            </a> berfungsi untuk mencetak pemakaian bmhp.
+       </td>
+     </tr>
+</table>
+</p>
+
