5ec9512a20e8746b7117d05aa132ec2cbff3a3dd
Iqbal Laksana <iqbal_laksana01@yahoo.co.id>
2016-04-20 10:11:27 +0900

    ABE-825: Fail Checklist Pada Menu : Master > Diagnosa Obat

--- protected/modules/farmasiApotek/controllers/KasuspenyakitobatMController.php
+++ protected/modules/farmasiApotek/controllers/KasuspenyakitobatMController.php
@@ -172,7 +172,7 @@ class KasuspenyakitobatMController extends MyAuthController
             $mpdf->WriteHTML($stylesheet,1);  
             $mpdf->AddPage($posisi,'','','','',15,15,15,15,15,15);
             $mpdf->WriteHTML($this->renderPartial('Print',array('model'=>$model,'judulLaporan'=>$judulLaporan,'caraPrint'=>$caraPrint),true));
-            $mpdf->Output();
+            $mpdf->Output($judulLaporan.'-'.date('Y-m-d').'.pdf','I');
         }                 
     }
 
