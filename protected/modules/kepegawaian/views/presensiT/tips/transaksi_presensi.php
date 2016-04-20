bc84351a48476c5bef95dc4c25ed6b3ffb4f718f
Iqbal Laksana <iqbal_laksana01@yahoo.co.id>
2016-04-20 14:37:47 +0900

    ABE-946: fail cheklist

--- protected/modules/kepegawaian/views/presensiT/tips/transaksi_presensi.php
+++ protected/modules/kepegawaian/views/presensiT/tips/transaksi_presensi.php
@@ -1,37 +1,33 @@
+ Bagian dengan tanda bintang <span class="required">*</span> harus diisi.
 <ol class="row-fluid">
-    <div class="span6">
-        <li>
-            Bagian dengan tanda bintang <span class="required">*</span> harus diisi.
-        </li>
+    <div class="span6">        
         <li>
             Icon <i class="icon-calendar"></i> untuk menentukan tanggal presensi.
         </li>
         <li>
             Icon <i class="icon-time"></i> untuk menentukan waktu presensi.
-        </li>
+        </li>       
         <li>
-            Icon <i class="icon-list-alt"></i> berfungsi untuk mencari dan menampilkan daftar data sesuai yang diketikan.
-        </li>
-        <li>
-            Icon <span class="add-on"><a id="" href="javascript:void(0);"><i class="icon-list-alt"></i><i class="icon-search"></i></a></span> 
+            Icon <span class="add-on"><a id="" href="javascript:void(0);"><i class="icon-list"></i><i class="icon-search"></i></a></span> 
             berfungsi untuk mencari dan menampilkan daftar data sesuai yang diketikan serta menampilkan dialog box jika diklik.
+        </li>
+         
+        <li>
+            Tombol  <div class="btn btn-primary" type="button"><i class="icon-ok icon-white"></i> Simpan</div>
+            berfungsi untuk menyimpan.
         </li>
     </div>
     <div class="span6">
         <li>
-            Tombol  <div class="btn btn-primary" name="yt0" type="button"><i class="icon-ok icon-white"></i> Simpan</div>
-            berfungsi untuk menyimpan.
-        </li>
-        <li>
-            Tombol  <div class="btn btn-danger" name="yt0" type="button"><i class="icon-refresh icon-white"></i> Ulang</div>
+            Tombol  <div class="btn btn-danger"type="button"><i class="icon-refresh icon-white"></i> Ulang</div>
             berfungsi untuk melakukan transaksi ulang.
         </li>
         <li>
-            Tombol  <div class="btn btn-info" name="yt0" type="button"><i class="icon-print icon-white"></i></div> dapat digunakan (sedang aktif)
+            Tombol  <div class="btn btn-info" type="button"><i class="icon-print icon-white"></i></div> dapat digunakan (sedang aktif)
             sedangkan tombol <div class="btn btn-info" name="yt0" type="button" disabled="disabled"><i class="icon-print icon-white"></i></div> tidak dapat digunakan (tidak aktif).
         </li>
         <li>
-            Tombol  <div class="btn btn-info" name="yt0" type="button"><i class="icon-print icon-white"></i> Print</div>
+            Tombol  <div class="btn btn-info" type="button"><i class="icon-print icon-white"></i> Print</div>
             berfungsi untuk mencetak presensi.
         </li>
     </div>
