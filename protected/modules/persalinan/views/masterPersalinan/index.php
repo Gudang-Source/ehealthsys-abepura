3919e471a5cdb7dd3ab089a0f03d63043cad978b
Deni Hamdani <pii.deni.prg@gmail.com>
2016-04-28 17:02:48 +0900

    ABE-975 Master Lookup Persalinan

---
+++ protected/modules/persalinan/views/masterPersalinan/index.php
@@ -0,0 +1,8 @@
+<div class="white-container">
+    <legend class="rim2">Master <b>Persalinan</b></legend>
+    <?php $this->renderPartial('_tabMenu',array()); ?>
+    <?php $this->renderPartial('_jsFunctions',array()); ?>
+    <div>
+        <iframe class="biru" id="frame" src="" width='100%' frameborder="0" style="overflow-y:scroll" ></iframe>
+    </div>
+</div>
