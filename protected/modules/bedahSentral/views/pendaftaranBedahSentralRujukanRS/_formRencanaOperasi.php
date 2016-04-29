10dcccf4889e28c7a05f3f7316e282e87a1f9bdd
Iqbal Laksana <iqbal_laksana01@yahoo.co.id>
2016-04-29 13:33:14 +0900

    ABE-1000: Ubah Form Rencana Operasi

--- protected/modules/bedahSentral/views/pendaftaranBedahSentralRujukanRS/_formRencanaOperasi.php
+++ protected/modules/bedahSentral/views/pendaftaranBedahSentralRujukanRS/_formRencanaOperasi.php
@@ -2,12 +2,16 @@
 	<?php echo CHtml::label('Tanggal <span class="required">*</span>','Tanggal',array('class'=>'control-label required')); ?>
 	<div class="controls">
 			<?php   
+                                        $format = new MyFormatter;
+                                        $modRencanaOperasi->tglrencanaoperasi = $format->formatDateTimeForUser($modRencanaOperasi->tglrencanaoperasi);
+                                        
 					$this->widget('MyDateTimePicker',array(
 									'model'=>$modRencanaOperasi,
 									'attribute'=>'tglrencanaoperasi',
 									'mode'=>'datetime',
 									'options'=> array(
 										'dateFormat'=>Params::DATE_FORMAT,
+                                                                            
 									),
 									'htmlOptions'=>array('readonly'=>true,'class'=>'dtPicker3', 'onkeypress'=>"return $(this).focusNextInputField(event)"),
 			)); ?>
@@ -21,24 +25,44 @@
 											'class'=>'span3'
 								   )); ?>
 <div class="control-group">
-	<?php echo CHtml::label('Dokter Pelaksana 1 <span class="required">*</span>','dokter',array('class'=>'control-label required')) ?>
+	<?php echo CHtml::label('Operator <span class="required">*</span>','dokter',array('class'=>'control-label required')) ?>
 	<div class="controls">
-		<?php echo $form->dropDownList($modRencanaOperasi,'dokterpelaksana1_id', CHtml::listData($modRencanaOperasi->getDokterItems(Params::RUANGAN_ID_BEDAH), 'pegawai_id', 'namaLengkap') ,array('empty'=>'-- Pelaksana 1 --','onkeypress'=>"return $(this).focusNextInputField(event)",'class'=>'span3')); ?>
+		<?php echo $form->dropDownList($modRencanaOperasi,'dokterpelaksana1_id', CHtml::listData($modRencanaOperasi->getDokterItems(Params::RUANGAN_ID_BEDAH), 'pegawai_id', 'namaLengkap') ,array('empty'=>'-- Pilih --','onkeypress'=>"return $(this).focusNextInputField(event)",'class'=>'span3')); ?>
 		<?php echo $form->error($modRencanaOperasi, 'dokterpelaksana1_id'); ?>
 	</div>
 </div>
 <div class="control-group">
-	<?php echo CHtml::label('Dokter Pelaksana 2','dokter',array('class'=>'control-label')) ?>
+	<?php echo CHtml::label('Asisten Operator','dokter',array('class'=>'control-label')) ?>
 	<div class="controls">
-		<?php echo $form->dropDownList($modRencanaOperasi,'dokterpelaksana2_id', CHtml::listData($modRencanaOperasi->getDokterItems(Params::RUANGAN_ID_BEDAH), 'pegawai_id', 'namaLengkap') ,array('empty'=>'-- Pelaksana 2 --','onkeypress'=>"return $(this).focusNextInputField(event)",'class'=>'span3')); ?>
+		<?php echo $form->dropDownList($modRencanaOperasi,'dokterpelaksana2_id', CHtml::listData($modRencanaOperasi->getDokterParamedisItems(Params::RUANGAN_ID_BEDAH), 'pegawai_id', 'namaLengkap') ,array('empty'=>'-- Pilih --','onkeypress'=>"return $(this).focusNextInputField(event)",'class'=>'span3')); ?>
 		<?php echo $form->error($modRencanaOperasi, 'dokterpelaksana2_id'); ?>
 	</div>
 </div>
+<div class="control-group">
+    <?php echo CHtml::label('Anastesi','dokteranastesi_id',array('class'=>'control-label')) ?>
+    <div class="controls">
+        <?php echo $form->dropDownList($modRencanaOperasi,'dokteranastesi_id', CHtml::listData($modRencanaOperasi->getDokterItems(Params::RUANGAN_ID_BEDAH), 'pegawai_id', 'namaLengkap') ,array('empty'=>'-- Pilih --','onkeypress'=>"return $(this).focusNextInputField(event)",'class'=>'span3')); ?>
+        </div>
+</div>
+<div class="control-group">
+    <?php echo CHtml::label('Penata Anastesi','paramedis_id',array('class'=>'control-label')) ?>
+    <div class="controls">
+        <?php echo $form->dropDownList($modRencanaOperasi,'paramedis_id', CHtml::listData($modRencanaOperasi->getParamedisItems(Params::RUANGAN_ID_BEDAH), 'pegawai_id', 'nama_pegawai') ,array('empty'=>'-- Pilih --','onkeypress'=>"return $(this).focusNextInputField(event)",'class'=>'span3')); ?>
+        </div>
+</div>
+<div class="control-group">
+    <?php echo CHtml::label('Perawat Anastesi','suster_id',array('class'=>'control-label')) ?>
+    <div class="controls">
+        <?php echo $form->dropDownList($modRencanaOperasi,'suster_id', CHtml::listData($modRencanaOperasi->getParamedisItems(Params::RUANGAN_ID_BEDAH), 'pegawai_id', 'nama_pegawai') ,array('empty'=>'-- Pilih --','onkeypress'=>"return $(this).focusNextInputField(event)",'class'=>'span3')); ?>
+        </div>
+</div>
+<div class="control-group">
+    <?php echo CHtml::label('Perawat Instrument','bidan_id',array('class'=>'control-label')) ?>
+    <div class="controls">
+        <?php echo $form->dropDownList($modRencanaOperasi,'bidan_id', CHtml::listData($modRencanaOperasi->getParamedisItems(Params::RUANGAN_ID_BEDAH), 'pegawai_id', 'nama_pegawai') ,array('empty'=>'-- Pilih --','onkeypress'=>"return $(this).focusNextInputField(event)",'class'=>'span3')); ?>
+        </div>
+</div>
 
-<?php echo $form->dropDownListRow($modRencanaOperasi,'dokteranastesi_id', CHtml::listData($modRencanaOperasi->getDokterItems(Params::RUANGAN_ID_BEDAH), 'pegawai_id', 'namaLengkap') ,array('empty'=>'-- Pilih --','onkeypress'=>"return $(this).focusNextInputField(event)",'class'=>'span3')); ?>
-<?php echo $form->dropDownListRow($modRencanaOperasi,'paramedis_id', CHtml::listData($modRencanaOperasi->getParamedisItems(Params::RUANGAN_ID_BEDAH), 'pegawai_id', 'nama_pegawai') ,array('empty'=>'-- Pilih --','onkeypress'=>"return $(this).focusNextInputField(event)",'class'=>'span3')); ?>
-<?php echo $form->dropDownListRow($modRencanaOperasi,'suster_id', CHtml::listData($modRencanaOperasi->getParamedisItems(Params::RUANGAN_ID_BEDAH), 'pegawai_id', 'nama_pegawai') ,array('empty'=>'-- Pilih --','onkeypress'=>"return $(this).focusNextInputField(event)",'class'=>'span3')); ?>
-<?php echo $form->dropDownListRow($modRencanaOperasi,'bidan_id', CHtml::listData($modRencanaOperasi->getParamedisItems(Params::RUANGAN_ID_BEDAH), 'pegawai_id', 'nama_pegawai') ,array('empty'=>'-- Pilih --','onkeypress'=>"return $(this).focusNextInputField(event)",'class'=>'span3')); ?>
 <?php echo $form->dropDownListRow($modRencanaOperasi,'statusoperasi', LookupM::getItems('statusoperasi'),  
 								  array('empty'=>'-- Pilih --', 'onkeypress'=>"return $(this).focusNextInputField(event)",'class'=>'span3','disabled'=>true
 										)); ?>   
