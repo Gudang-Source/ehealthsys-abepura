02dfd3798750cee570366d103aebf0c878b46517
Iqbal Laksana <iqbal_laksana01@yahoo.co.id>
2016-05-10 10:10:52 +0900

    ABE-1058: Penyesuaian pada Informasi Tarif (modul bedah sentral)

--- protected/modules/bedahSentral/views/informasiTarif/index.php
+++ protected/modules/bedahSentral/views/informasiTarif/index.php
@@ -9,20 +9,29 @@
             'template'=>"{summary}\n{items}\n{pager}",
             'itemsCssClass'=>'table table-striped table-condensed',
             'columns'=>array(
+                'jenistarif_nama',
                 'kelompoktindakan_nama',
+                'komponenunit_nama',
                 'kategoritindakan_nama',
                 'daftartindakan_nama',
                 'kelaspelayanan_nama',
                 array(
                     'name'=>'tarifTotal',
                     'value'=>'$this->grid->getOwner()->renderPartial(\'_tarifTotal\',array(\'kelaspelayanan_id\'=>$data->kelaspelayanan_id,\'daftartindakan_id\'=>$data->daftartindakan_id),true)',
+                    'htmlOptions'=>array('style'=>'text-align:right;'),
                 ),
-                'persencyto_tind',
-                'persendiskon_tind',
+                 array(
+                        'name'=>'persencyto_tind',
+                        'htmlOptions'=>array('style'=>'text-align: right'),
+                    ), 
+                array(
+                        'name'=>'persendiskon_tind',
+                        'htmlOptions'=>array('style'=>'text-align: right'),
+                    ),
                 array(
                     'name'=>'Komponen Tarif',
                     'type'=>'raw',
-                    'value'=>'CHtml::link("<i class=\'icon-form-komtarif\'></i> ",Yii::app()->controller->createUrl("'.Yii::app()->controller->id.'/detailsTarif",array("kelaspelayanan_id"=>$data->kelaspelayanan_id,"daftartindakan_id"=>$data->daftartindakan_id, "kategoritindakan_id"=>$data->kategoritindakan_id)) ,array("title"=>"Klik Untuk Melihat Detail Tarif","target"=>"iframe", "onclick"=>"$(\"#dialogDetailsTarif\").dialog(\"open\");", "rel"=>"tooltip"))', 'htmlOptions'=>array('style'=>'text-align: center; width:40px')
+                    'value'=>'CHtml::link("<i class=\'icon-form-komtarif\'></i> ",Yii::app()->controller->createUrl("'.Yii::app()->controller->id.'/detailsTarif",array("kelaspelayanan_id"=>$data->kelaspelayanan_id,"daftartindakan_id"=>$data->daftartindakan_id, "kategoritindakan_id"=>$data->kategoritindakan_id, "jenistarif_id"=>$data->jenistarif_id)) ,array("title"=>"Klik Untuk Melihat Detail Tarif","target"=>"iframe", "onclick"=>"$(\"#dialogDetailsTarif\").dialog(\"open\");", "rel"=>"tooltip"))', 'htmlOptions'=>array('style'=>'text-align: center; width:40px')
                 ),
 
             ),
@@ -42,16 +51,19 @@
         <table width="100%">
             <tr>
                 <td>
-                    <?php echo $form->dropDownListRow($modTarifTindakanRuanganV,'kategoritindakan_id',CHtml::listData(KategoritindakanM::model()->findAll('kategoritindakan_aktif = true ORDER BY kategoritindakan_nama'), 'kategoritindakan_id', 'kategoritindakan_nama'),array('class'=>'span3', 'onkeypress'=>"return $(this).focusNextInputField(event)",'empty'=>'-- Pilih --')); ?>
+                    <?php echo $form->dropDownListRow($modTarifTindakanRuanganV, 'jenistarif_id', CHtml::listData(JenistarifM::model()->findAllByAttributes(array('jenistarif_aktif'=>true), array('order'=>'jenistarif_nama ASC')), 'jenistarif_id', 'jenistarif_nama'), array('class'=>'span3', 'empty'=>'-- Pilih --')); ?>
+                    <?php echo $form->dropDownListRow($modTarifTindakanRuanganV, 'kelompoktindakan_id', CHtml::listData(KelompoktindakanM::model()->findAllByAttributes(array('kelompoktindakan_aktif'=>true), array('order'=>'kelompoktindakan_nama ASC')), 'kelompoktindakan_id', 'kelompoktindakan_nama'), array('class'=>'span3', 'empty'=>'-- Pilih --')); ?>
                 </td>
                 <td>
-                    <?php echo $form->dropDownListRow($modTarifTindakanRuanganV,'kelaspelayanan_id',CHtml::listData(KelaspelayananM::model()->findAll('kelaspelayanan_aktif = true ORDER BY kelaspelayanan_nama'), 'kelaspelayanan_id', 'kelaspelayanan_nama'),array('class'=>'span3', 'onkeypress'=>"return $(this).focusNextInputField(event)",'empty'=>'-- Pilih --')); ?>
+                    <?php echo $form->dropDownListRow($modTarifTindakanRuanganV, 'komponenunit_id', CHtml::listData(KomponenunitM::model()->findAllByAttributes(array('komponenunit_aktif'=>true), array('order'=>'komponenunit_nama ASC')), 'komponenunit_id', 'komponenunit_nama'), array('class'=>'span3', 'empty'=>'-- Pilih --')); ?>
+                    <?php echo $form->dropDownListRow($modTarifTindakanRuanganV,'kategoritindakan_id',CHtml::listData($modTarifTindakanRuanganV->getKategoritindakanItems(), 'kategoritindakan_id', 'kategoritindakan_nama'),array('class'=>'span3', 'onkeypress'=>"return $(this).focusNextInputField(event)",'empty'=>'-- Pilih --')); ?>
                 </td>
                 <td>
-                    <?php echo $form->textFieldRow($modTarifTindakanRuanganV,'daftartindakan_id',array( 'onkeypress'=>"return $(this).focusNextInputField(event);", 'maxlength'=>30, 'autofocus'=>true, 'placeholder'=>'Ketik nama daftar tindakan')); ?>
+                    <?php echo $form->dropDownListRow($modTarifTindakanRuanganV,'kelaspelayanan_id',CHtml::listData($modTarifTindakanRuanganV->getKelasPelayananItems(), 'kelaspelayanan_id', 'kelaspelayanan_nama'),array('class'=>'span3', 'onkeypress'=>"return $(this).focusNextInputField(event)",'empty'=>'-- Pilih --')); ?>
+                    <?php echo $form->textFieldRow($modTarifTindakanRuanganV,'daftartindakan_nama',array( 'onkeypress'=>"return $(this).focusNextInputField(event);", 'maxlength'=>30, 'placeholder'=>'Ketik nama daftar tindakan')); ?>
                 </td>
             </tr>
-        </table>
+        </table>          
         <div class="form-actions">
             <?php echo CHtml::htmlButton(Yii::t('mds','{icon} Search',array('{icon}'=>'<i class="icon-search icon-white"></i>')),array('class'=>'btn btn-primary', 'type'=>'submit')); ?>
             <?php echo CHtml::link(Yii::t('mds','{icon} Ulang',array('{icon}'=>'<i class="icon-refresh icon-white"></i>')), 
@@ -59,7 +71,7 @@
                                     array('class'=>'btn btn-danger',
                                           'onclick'=>'myConfirm("Apakah anda ingin mengulang ini?","Perhatian!",function(r){if(r) window.location = window.location.href;}); return false;'));  ?>
             <?php 
-              $content = $this->renderPartial('../tips/informasi',array(),true);
+              $content = $this->renderPartial('../tips/informasiTarif',array(),true);
                            $this->widget('UserTips',array('type'=>'admin','content'=>$content));
             ?>
         </div>
@@ -74,8 +86,8 @@ $this->beginWidget('zii.widgets.jui.CJuiDialog', array(
                         'options'=>array(
                         'title'=>'Komponen Tarif',
                         'autoOpen'=>false,
-                        'width'=>250,
-                        'height'=>300,
+                        'width'=>300,
+                        'height'=>350,
                         'resizable'=>false,
                         'scroll'=>false    
                          ),
