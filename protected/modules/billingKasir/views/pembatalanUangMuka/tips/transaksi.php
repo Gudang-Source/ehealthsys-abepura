7c80c7e3582cefc96376a3c9d4e195b218c298fd
Iqbal Laksana <iqbal_laksana01@yahoo.co.id>
2016-05-17 09:39:11 +0900

    ABE-1124: Transaki Pembatalan Uang Muka

--- protected/modules/billingKasir/views/pembatalanUangMuka/tips/transaksi.php
+++ protected/modules/billingKasir/views/pembatalanUangMuka/tips/transaksi.php
@@ -2,35 +2,27 @@
 <p>
 <table border="0" style="padding :none;">
   <tr>
-    <td>1. </td>
-    <td>Icon <i class="icon-calendar"></i><i class="icon-time"></i> untuk menentukan tanggal dan waktu pendaftaran.</td>
+      <td style="vertical-align:middle">1. </td>
+    <td>Icon <i class="icon-calendar"></i><i class="icon-time"></i>berfungsi untuk menentukan tanggal dan waktu.</td>
   </tr>
   <tr>
-      <td>2. </td>
-        <td>Gunakan icon ini <i class="icon-list-alt"></i> berfungsi untuk menampilkan list.</td>
+      <td style="vertical-align:middle">2. </td>
+        <td>Icon <i class="icon-list"></i> berfungsi untuk menampilkan list data sesuai dengan yang di ketikkan.</td>
   </tr>
-     <tr>
-	 <td>3. </td>
-        <td>Gunakan tombol ini <a class="btn btn-info" href="#" onclick="printKasir($('#FAPendaftaranT_pendaftaran_id').val());return false">
-<i class="icon-print icon-white"></i>
-Print
-</a> berfungsi untuk mencetak data.</td>
- </tr>
-     <tr>  <td>4. </td>
-        <td>Gunakan tombol ini  <button class="btn btn-primary" name="yt0" onkeypress="return formSubmit(this,event)" type="submit">
+     
+     <tr>  <td style="vertical-align:middle">3. </td>
+        <td>Gunakan tombol ini  <button class="btn btn-primary" name="yt0" otype="button">
 <i class="icon-ok icon-white"></i>
 Simpan
-</button>
-Cari
 </button> berfungsi untuk menyimpan</td>
  </tr> 
  
  <tr>
-    <td>5. </td>
-    <td>Gunakan tombol ini <a class="btn btn-danger" href="/simrs/index.php?r=rawatInap/pasienRawatInap/formTindakLanjutDariPasienRI">
+    <td style="vertical-align:middle">4. </td>
+    <td>Gunakan tombol ini <a class="btn btn-danger" >
 <i class="icon-refresh icon-white"></i>
 Ulang
-</a> untunk mengulang kembali inputan.</td>
+</a> untuk mengulang kembali inputan.</td>
   </tr>
 </table>
 </p>
