f7a76cedeeffd8226958034bdc657f7dfa611f77
Deni Hamdani <pii.deni.prg@gmail.com>
2016-05-18 14:42:36 +0900

    1043 Fix error Informasi Permintaan Penawaran

--- protected/modules/gudangUmum/controllers/InformasiPermintaanPenawaranGUController.php
+++ protected/modules/gudangUmum/controllers/InformasiPermintaanPenawaranGUController.php
@@ -1,6 +1,6 @@
 <?php
 Yii::import('pengadaan.controllers.InformasiPermintaanPenawaranController');
-
+Yii::import('pengadaan.models.*');
 class InformasiPermintaanPenawaranGUController extends InformasiPermintaanPenawaranController {
     //put your code here
 }
