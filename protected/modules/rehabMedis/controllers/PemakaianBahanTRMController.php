65a3fba829b8f6bec90cb8712549561d310fee7a
Iqbal Laksana <iqballaksana01@gmail.com>
2016-05-24 17:11:38 +0900

    ABE-1091: Penyesuaian Menu yang sudah di Develop (modul rehabMedis)

--- protected/modules/rehabMedis/controllers/PemakaianBahanTRMController.php
+++ protected/modules/rehabMedis/controllers/PemakaianBahanTRMController.php
@@ -1,7 +1,17 @@
-<?php
-Yii::import('rawatJalan.controllers.PemakaianBahanController');
-Yii::import('rawatJalan.models.*');
+    <?php
+Yii::import('laboratorium.controllers.PemakaianBahanController');
+Yii::import('laboratorium.models.LBObatalkespasienT');
+Yii::import('laboratorium.models.LBObatalkesM');
+Yii::import('laboratorium.models.LBHasilPemeriksaanLabT');
+Yii::import('laboratorium.models.LBPasienmasukpenunjangT');
+Yii::import('laboratorium.models.LBPasienMasukPenunjangV');
 class PemakaianBahanTRMController extends PemakaianBahanController
 {
-        
+  //  public $path_view = "rehabMedis.views.pemakaianBahanRM.";
+  //  public $path_tips = "laboratorium.views.pemakaianBahan.";
+    public $path_view_bahan = "laboratorium.views.pemakaianBahan.";
+    public $path_view_bmhp = "laboratorium.views.pemakaianBmhp.";
+    
+    
+    
 }
