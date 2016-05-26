399e8542de8a7c84ff863dfaa2f46d15457c6b8c
Deni Hamdani <pii.deni.prg@gmail.com>
2016-05-26 09:32:41 +0900

    ABE-1038 Inventarisasi Peralatan dan Mesin

--- protected/modules/manajemenAset/views/invperalatanT/_form.php
+++ protected/modules/manajemenAset/views/invperalatanT/_form.php
@@ -1,3 +1,5 @@
+<?php Yii::app()->clientScript->registerScriptFile(Yii::app()->baseUrl.'/js/accounting2.js', CClientScript::POS_END); ?>
+<?php Yii::app()->clientScript->registerScriptFile(Yii::app()->baseUrl.'/js/form2.js', CClientScript::POS_END); ?>
 
 <?php Yii::app()->clientScript->registerScriptFile(Yii::app()->baseUrl.'/js/form.js'); ?>
 <?php $form=$this->beginWidget('ext.bootstrap.widgets.BootActiveForm',array(
@@ -22,20 +24,20 @@
             <table width="100%">
                 <tr>
                     <td>
-                        <?php echo $form->dropDownListRow($model,'pemilikbarang_id',CHtml::listData(PemilikbarangM::model()->findAll(), 'pemilikbarang_id', 'pemilikbarang_nama'),array('class'=>'span2', 'onkeypress'=>"return $(this).focusNextInputField(event)",'empty'=>'-- Pilih --')); ?>
+                        <?php echo $form->dropDownListRow($model,'pemilikbarang_id',CHtml::listData(PemilikbarangM::model()->findAll(array('order'=>'pemilikbarang_kode')), 'pemilikbarang_id', 'pemilikbarang_nama'),array('class'=>'span2', 'onkeypress'=>"return $(this).focusNextInputField(event)",'empty'=>'-- Pilih --')); ?>
                         <?php echo $form->hiddenField($model,'barang_id'); ?>
                         <?php echo $form->hiddenField($model,'barang_nama',array('class'=>'span3', 'onkeypress'=>"return $(this).focusNextInputField(event);")); ?>
                         <?php echo $form->dropDownListRow($model,'asalaset_id',CHtml::listData(AsalasetM::model()->findAll(), 'asalaset_id', 'asalaset_nama'),array('class'=>'span2', 'onkeypress'=>"return $(this).focusNextInputField(event)",'empty'=>'-- Pilih --')); ?>
-                        <?php echo $form->dropDownListRow($model,'lokasi_id',CHtml::listData(LokasiasetM::model()->findAll(), 'lokasi_id', 'lokasiaset_namalokasi'),array('class'=>'span2', 'onkeypress'=>"return $(this).focusNextInputField(event)",'empty'=>'-- Pilih --')); ?>
-                        <?php echo $form->textFieldRow($model,'invperalatan_kode',array('class'=>'span1', 'onkeypress'=>"return $(this).focusNextInputField(event);", 'maxlength'=>50)); ?>
-                        <?php echo $form->textFieldRow($model,'invperalatan_noregister',array('class'=>'span2', 'onkeypress'=>"return $(this).focusNextInputField(event);", 'maxlength'=>50)); ?>
-                        <?php echo $form->textFieldRow($model,'invperalatan_namabrg',array('class'=>'span2', 'onkeypress'=>"return $(this).focusNextInputField(event);", 'maxlength'=>100)); ?>
-                        <?php echo $form->textFieldRow($model,'invperalatan_merk',array('class'=>'span3', 'onkeypress'=>"return $(this).focusNextInputField(event);", 'maxlength'=>50)); ?>
-                        <?php echo $form->textFieldRow($model,'invperalatan_ukuran',array('class'=>'span1 numbersOnly', 'onkeypress'=>"return $(this).focusNextInputField(event);", 'maxlength'=>50)); ?>
-                        <?php echo $form->textFieldRow($model,'invperalatan_bahan',array('class'=>'span3', 'onkeypress'=>"return $(this).focusNextInputField(event);", 'maxlength'=>100)); ?>
+                        <?php echo $form->dropDownListRow($model,'lokasi_id',CHtml::listData(LokasiasetM::model()->findAll(array('order' => 'lokasiaset_namalokasi')), 'lokasi_id', 'lokasiaset_namalokasi'),array('class'=>'span2', 'onkeypress'=>"return $(this).focusNextInputField(event)",'empty'=>'-- Pilih --')); ?>
+                        <?php echo $form->textFieldRow($model,'invperalatan_kode',array('class'=>'span2 all-caps', 'onkeypress'=>"return $(this).focusNextInputField(event);", 'maxlength'=>50)); ?>
+                        <?php echo $form->textFieldRow($model,'invperalatan_noregister',array('class'=>'span2 all-caps', 'onkeypress'=>"return $(this).focusNextInputField(event);", 'maxlength'=>50)); ?>
+                        <?php echo $form->textFieldRow($model,'invperalatan_namabrg',array('class'=>'span2 all-caps', 'onkeypress'=>"return $(this).focusNextInputField(event);", 'maxlength'=>100)); ?>
+                        <?php echo $form->textFieldRow($model,'invperalatan_merk',array('class'=>'span3 all-caps', 'onkeypress'=>"return $(this).focusNextInputField(event);", 'maxlength'=>50)); ?>
+                        <?php echo $form->textFieldRow($model,'invperalatan_ukuran',array('class'=>'span1 numbersOnly', 'onkeypress'=>"return $(this).focusNextInputField(event);", 'maxlength'=>50, 'style'=>'text-align: right')); ?>
+                        <?php echo $form->textFieldRow($model,'invperalatan_bahan',array('class'=>'span3 all-caps', 'onkeypress'=>"return $(this).focusNextInputField(event);", 'maxlength'=>100)); ?>
                     </td>
                     <td>
-                        <?php echo $form->textFieldRow($model,'invperalatan_thnpembelian',array('class'=>'span1 numbersOnly', 'onkeypress'=>"return $(this).focusNextInputField(event);", 'maxlength'=>5)); ?>
+                        <?php echo $form->textFieldRow($model,'invperalatan_thnpembelian',array('class'=>'span1 numbersOnly', 'onkeypress'=>"return $(this).focusNextInputField(event);", 'maxlength'=>5, 'style'=>'text-align: right')); ?>
                         <div class="control-group ">
                             <?php echo $form->labelEx($model,'invperalatan_tglguna', array('class'=>'control-label')) ?>
                             <div class="controls">
@@ -55,23 +57,23 @@
                                 <?php echo $form->error($model, 'invperalatan_tglguna'); ?>
                             </div>
                         </div>
-                        <?php echo $form->textFieldRow($model,'invperalatan_nopabrik',array('class'=>'span2', 'onkeypress'=>"return $(this).focusNextInputField(event);", 'maxlength'=>50)); ?>
-                        <?php echo $form->textFieldRow($model,'invperalatan_norangka',array('class'=>'span2', 'onkeypress'=>"return $(this).focusNextInputField(event);", 'maxlength'=>50)); ?>
-                        <?php echo $form->textFieldRow($model,'invperalatan_nomesin',array('class'=>'span2', 'onkeypress'=>"return $(this).focusNextInputField(event);", 'maxlength'=>50)); ?>
-                        <?php echo $form->textFieldRow($model,'invperalatan_nopolisi',array('class'=>'span2', 'onkeypress'=>"return $(this).focusNextInputField(event);", 'maxlength'=>50)); ?>
-                        <?php echo $form->textFieldRow($model,'invperalatan_nobpkb',array('class'=>'span3', 'onkeypress'=>"return $(this).focusNextInputField(event);", 'maxlength'=>50)); ?>
-                        <?php echo $form->textFieldRow($model,'invperalatan_harga',array('class'=>'span2 numbersOnly', 'onkeypress'=>"return $(this).focusNextInputField(event);")); ?>
-                        <?php echo $form->textFieldRow($model,'invperalatan_akumsusut',array('class'=>'span2', 'onkeypress'=>"return $(this).focusNextInputField(event);")); ?>
+                        <?php echo $form->textFieldRow($model,'invperalatan_nopabrik',array('class'=>'span2 all-caps', 'onkeypress'=>"return $(this).focusNextInputField(event);", 'maxlength'=>50)); ?>
+                        <?php echo $form->textFieldRow($model,'invperalatan_norangka',array('class'=>'span2 all-caps', 'onkeypress'=>"return $(this).focusNextInputField(event);", 'maxlength'=>50)); ?>
+                        <?php echo $form->textFieldRow($model,'invperalatan_nomesin',array('class'=>'span2 all-caps', 'onkeypress'=>"return $(this).focusNextInputField(event);", 'maxlength'=>50)); ?>
+                        <?php echo $form->textFieldRow($model,'invperalatan_nopolisi',array('class'=>'span2 all-caps', 'onkeypress'=>"return $(this).focusNextInputField(event);", 'maxlength'=>50)); ?>
+                        <?php echo $form->textFieldRow($model,'invperalatan_nobpkb',array('class'=>'span3 all-caps', 'onkeypress'=>"return $(this).focusNextInputField(event);", 'maxlength'=>50)); ?>
+                        <?php echo $form->textFieldRow($model,'invperalatan_harga',array('class'=>'span2 integer2', 'onkeypress'=>"return $(this).focusNextInputField(event);")); ?>
+                        <?php echo $form->textFieldRow($model,'invperalatan_akumsusut',array('class'=>'span2 numbersOnly', 'onkeypress'=>"return $(this).focusNextInputField(event);", 'style'=>'text-align: right;')); ?>
                     </td>
                     <td>
                         <?php echo $form->textAreaRow($model,'invperalatan_ket',array('rows'=>4, 'cols'=>50, 'class'=>'span3', 'onkeypress'=>"return $(this).focusNextInputField(event);")); ?>
                         <?php echo $form->textFieldRow($model,'invperalatan_kapasitasrata',array('class'=>'span3', 'onkeypress'=>"return $(this).focusNextInputField(event);", 'maxlength'=>10)); ?>
                         <?php echo $form->checkBoxRow($model,'invperalatan_ijinoperasional', array('onkeypress'=>"return $(this).focusNextInputField(event);")); ?>
-                        <?php echo $form->textFieldRow($model,'invperalatan_serftkkalibrasi',array('class'=>'span3', 'onkeypress'=>"return $(this).focusNextInputField(event);", 'maxlength'=>20)); ?>
+                        <?php echo $form->textFieldRow($model,'invperalatan_serftkkalibrasi',array('class'=>'span3 all-caps', 'onkeypress'=>"return $(this).focusNextInputField(event);", 'maxlength'=>20)); ?>
                         <div class="control-group">
                             <?php echo $form->labelEx($model,'invperalatan_umurekonomis',array('class'=>'control-label')); ?>
                             <div class="controls">
-                                <?php echo $form->textField($model,'invperalatan_umurekonomis',array('class'=>'span1 numbersOnly', 'onkeypress'=>"return $(this).focusNextInputField(event);")); ?>
+                                <?php echo $form->textField($model,'invperalatan_umurekonomis',array('class'=>'span1 numbersOnly', 'onkeypress'=>"return $(this).focusNextInputField(event);", 'style'=>'text-align: right;')); ?>
                                 <?php echo CHtml::label('Tahun', 'tahun'); ?>
                             </div>
                         </div>
