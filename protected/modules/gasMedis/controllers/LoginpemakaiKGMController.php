4d81d11e84fa99ca86a48653966ed3d094a75561
Iqbal Laksana <iqballaksana01@gmail.com>
2016-05-27 17:21:31 +0900

    ABE-1217: fix tambah ganti password

---
+++ protected/modules/gasMedis/controllers/LoginpemakaiKGMController.php
@@ -0,0 +1,7 @@
+<?php
+
+Yii::import("rawatJalan.models.*");
+Yii::import("rawatJalan.controllers.LoginpemakaiKController");
+class LoginpemakaiKGMController extends LoginpemakaiKController {
+    //put your code here
+}
