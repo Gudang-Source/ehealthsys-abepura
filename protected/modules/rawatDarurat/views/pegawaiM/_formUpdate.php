91727639f832b0013865201a78c026cd5cc15dc3
Iqbal Laksana <iqballaksana01@gmail.com>
2016-05-28 09:53:49 +0900

    ABE-1201: Tambah/Ubah isian (surat registrasi + input kemampuan bahasa + warna kulit) final part

--- protected/modules/rawatDarurat/views/pegawaiM/_formUpdate.php
+++ protected/modules/rawatDarurat/views/pegawaiM/_formUpdate.php
@@ -206,9 +206,8 @@ $this->widget('bootstrap.widgets.BootAlert'); ?>
                 <legend class="rim">Data Lain - Lain</legend> 
                     <?php echo $form->dropDownListRow($model,'statuskepemilikanrumah_id',CHtml::listData($model->getStatuskepemilikanrumahItems(),'statuskepemilikanrumah_id','statuskepemilikanrumah_nama'),array('empty'=>'-- Pilih --','onkeyup'=>"return $(this).focusNextInputField(event)")); ?>
 
-                    <?php echo $form->textFieldRow($model,'kemampuanbahasa',array('onkeyup'=>"return $(this).focusNextInputField(event)",'placeholder'=>'contoh : Bahasa Indonesia, Inggris')); ?>
-
-                    <?php echo $form->textFieldRow($model,'warnakulit',array('onkeyup'=>"return $(this).focusNextInputField(event)",'placeholder'=>'contoh : Sawo Matang')); ?>                                                                                
+                   <?php echo $form->dropDownListRow($model,'kemampuanbahasa',LookupM::getItems('kemampuanbahasa'),array('empty'=>'-- Pilih --','onkeyup'=>"return $(this).focusNextInputField(event)")); ?>
+                    <?php echo $form->dropDownListRow($model,'warnakulit',LookupM::getItems('warnakulit'), array('empty'=>'-- Pilih --','onkeyup'=>"return $(this).focusNextInputField(event)",'placeholder'=>'contoh : Sawo Matang')); ?>
             </fieldset>                                                         
         </div>
         <?php /*
@@ -326,7 +325,7 @@ $this->widget('bootstrap.widgets.BootAlert'); ?>
                     <?php echo $form->error($model, 'tglditerima'); ?>
                 </div>
             </div>        
-
+            <?php echo $form->textFieldRow($model,'surattandaregistrasi',array('onkeyup'=>"return $(this).focusNextInputField(event)",'maxlength'=>100)); ?>
             <div class="control-group">
                 <?php echo $form->labelEx($model,'caraAmbilPhoto', array('class'=>'control-label')) ?>
                 <div class="controls">  
