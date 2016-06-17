e9ec3572cf7209819fce3484e4623fe766db49ea
Iqbal Laksana <iqballaksana01@gmail.com>
2016-06-17 13:23:04 +0900

    ABE-1372: Informasi Kartu Stok Obat Alkes pada modul yang belum ada

---
+++ protected/modules/rehabMedis/controllers/InformasiKartuStokObatAlkesRMController.php
@@ -0,0 +1,7 @@
+<?php
+
+Yii::import("gudangFarmasi.controllers.InformasiKartuStokObatAlkesController");
+YII::import("gudangFarmasi.models.*");
+class InformasiKartuStokObatAlkesRMController extends InformasiKartuStokObatAlkesController {
+    //put your code here
+}
