522ec1285fc6f12b5bccf2f9cf8054e82a022d48
Piranti <pi.informasi@gmail.com>
2016-06-22 08:15:50 +0900

    add index

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 ini_set('memory_limit', '-1');
 error_reporting(0); //UNTUK PRODUKSI
-//error_reporting(E_ALL | E_STRICT); //UNTUK DEVELOPMENT
+// error_reporting(E_ALL | E_STRICT); //UNTUK DEVELOPMENT
 
 // change the following paths if necessary
 $yii=dirname(__FILE__).'/yii1_10/framework/yiilite.php'; //UNTUK PRODUKSI
@@ -10,7 +10,7 @@ $config=dirname(__FILE__).'/protected/config/main.php';
 
 // remove the following lines when in production mode
 defined('YII_DEBUG') or define('YII_DEBUG',false); //UNTUK PRODUKSI
-//defined('YII_DEBUG') or define('YII_DEBUG',true); //UNTUK DEVELOPMENT
+// defined('YII_DEBUG') or define('YII_DEBUG',true); //UNTUK DEVELOPMENT
 
 // specify how many levels of call stack should be shown in each log message
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
