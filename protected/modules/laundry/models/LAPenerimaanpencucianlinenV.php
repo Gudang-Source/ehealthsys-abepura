ba23bab5f38c5a717c5ac13e5051d35478575ba7
Deni Hamdani <pii.deni.prg@gmail.com>
2016-07-13 11:59:57 +0700

    ABE-1528 Filter Penerimaan untuk Pencucian Linen

--- protected/modules/laundry/models/LAPenerimaanpencucianlinenV.php
+++ protected/modules/laundry/models/LAPenerimaanpencucianlinenV.php
@@ -15,44 +15,51 @@ class LAPenerimaanpencucianlinenV extends PenerimaanpencucianlinenV
 		$criteria=new CDbCriteria;
 		
 		$format = new MyFormatter();
+                
 		$this->tgl_awal		= $format->formatDateTimeForDb($this->tgl_awal);
 		$this->tgl_akhir	= $format->formatDateTimeForDb($this->tgl_akhir);
-		
 		$criteria->limit=1000;
 		if(!Yii::app()->request->isAjaxRequest){//data hanya muncul setelah melakukan pencarian
 			$criteria->limit = 0;
 		}
 		
-                if (!empty($this->tgl_awal) && !empty($this->tglakhir)) {
-                    $criteria->addBetweenCondition('DATE(tglpenerimaanlinen)', $this->tgl_awal, $this->tgl_akhir,true);
-                }
+                
 		if(!empty($this->instalasi_id)){
-			$criteria->addCondition('instalasi_id = '.$this->instalasi_id);
+			$criteria->addCondition('t.instalasi_id = '.$this->instalasi_id);
 		}
-		$criteria->compare('LOWER(instalasi_nama)',strtolower($this->instalasi_nama),true);
+		$criteria->compare('LOWER(t.instalasi_nama)',strtolower($this->instalasi_nama),true);
 		if(!empty($this->ruangan_id)){
-			$criteria->addCondition('ruangan_id = '.$this->ruangan_id);
+			$criteria->addCondition('t.ruangan_id = '.$this->ruangan_id);
 		}
-		$criteria->compare('LOWER(ruangan_nama)',strtolower($this->ruangan_nama),true);
+		$criteria->compare('LOWER(t.ruangan_nama)',strtolower($this->ruangan_nama),true);
 		
-		$criteria->compare('LOWER(nopenerimaanlinen)',strtolower($this->nopenerimaanlinen),true);
+		$criteria->compare('LOWER(t.nopenerimaanlinen)',strtolower($this->nopenerimaanlinen),true);
 		
                 if (empty($this->penerimaanlinen_id) && empty($this->perawatanlinen_id)) {
-                    if (!empty($this->jenisperawatan)){
-                            $criteria->compare('LOWER(jenisperawatanlinen)',strtolower($this->jenisperawatan));
-                    } else {
-                            $criteria->addCondition("jenisperawatanlinen = '".Params::JENISPERAWATAN_DEKONTAMINASI."'");
+                    if (!empty($this->tgl_awal) && !empty($this->tgl_akhir)) {
+                        $criteria->addBetweenCondition('DATE(t.tglpenerimaanlinen)', $this->tgl_awal, $this->tgl_akhir);
                     }
                     
-                    $criteria->addCondition("statusperawatanlinen = '".Params::STATUSPERAWATAN_SELESAI."'");
+                    if (!empty($this->jenisperawatan)){
+                            $criteria->compare('LOWER(t.jenisperawatanlinen)',strtolower($this->jenisperawatan));
+                    } else {
+                            $criteria->addCondition("t.jenisperawatanlinen = '".Params::JENISPERAWATAN_DEKONTAMINASI."'");
+                    }
+                    
+                    // $criteria->addCondition("t.statusperawatanlinen = '".Params::STATUSPERAWATAN_SELESAI."'");
                 } else if (!empty($this->penerimaanlinen_id)) {
-                    $criteria->group = $criteria->select = "penerimaanlinen_id, linen_id, penerimaanlinen_id, ruangan_nama, nopenerimaanlinen, kodelinen, namalinen, keteranganpenerimaanlinen_item, jenisperawatanlinen";
+                    $criteria->group = $criteria->select = "t.penerimaanlinen_id, t.linen_id, t.penerimaanlinen_id, t.ruangan_nama, t.nopenerimaanlinen, t.kodelinen, namalinen, t.keteranganpenerimaanlinen_item, t.jenisperawatanlinen";
                 }
                 
-                $criteria->compare('perawatanlinen_id', $this->perawatanlinen_id);
-                $criteria->compare('penerimaanlinen_id', $this->penerimaanlinen_id);
+                $criteria->compare('t.perawatanlinen_id', $this->perawatanlinen_id);
+                $criteria->compare('t.penerimaanlinen_id', $this->penerimaanlinen_id);
+                
+                $criteria->join = 'left join pencuciandetail_t p on p.penerimaanlinen_id = t.penerimaanlinen_id and p.linen_id = t.linen_id';
+                $criteria->addCondition('p.pencuciandetail_id is null');
                 
                 $criteria->limit = -1;
+                
+                // var_dump($criteria); die;
                 
 		return new CActiveDataProvider($this, array(
 				'criteria'=>$criteria,
