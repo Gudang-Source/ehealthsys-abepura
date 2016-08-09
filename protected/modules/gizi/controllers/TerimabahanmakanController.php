bf608f3bc82f40c665762c8ed8b13bec03ddef22
Deni Hamdani <pii.deni.prg@gmail.com>
2016-08-09 13:19:18 +0700

    ABE-1729 Form Terima Bahan Makanan

--- protected/modules/gizi/controllers/TerimabahanmakanController.php
+++ protected/modules/gizi/controllers/TerimabahanmakanController.php
@@ -210,6 +210,9 @@ class TerimabahanmakanController extends MyAuthController
         
         public function actionGetBahanMakananDariPenerimaan(){
         if (Yii::app()->request->isAjaxRequest){
+			Yii::app()->clientScript->scriptMap['*.js'] = false;
+            Yii::app()->clientScript->scriptMap['*.css'] = false;
+			
             $idBahan = $_POST['id'];
             $qty = $_POST['qty'];
             $ukuran = $_POST['ukuran'];
@@ -221,35 +224,18 @@ class TerimabahanmakanController extends MyAuthController
             $model = BahanmakananM::model()->with('golbahanmakanan')->findByPk($idBahan);
             $modDetail = new TerimabahandetailT();
             $subNetto = $qty*$model->harganettobahan;
+			
 //            $modDetail->satuanbahan[] = $satuanbahan;
-            $tr = '<tr>
-                    <td>'
-                        .CHtml::checkBox('checkList[]',true,array('class'=>'cekList','onclick'=>'hitungSemua()'))
-                        .CHtml::activeHiddenField($modDetail, '[0]golbahanmakanan_id', array('value'=>$model->golbahanmakanan_id))
-                        .CHtml::activeHiddenField($modDetail, '[0]bahanmakanan_id', array('value'=>$model->bahanmakanan_id))
-                        .CHtml::activeHiddenField($modDetail, '[0]harganettobhn', array('value'=>$model->harganettobahan,'class'=>'integer'))
-                        .CHtml::activeHiddenField($modDetail, '[0]jmlkemasan', array('value'=>$model->jmldlmkemasan,'class'=>'integer'))            
-                        .CHtml::activeHiddenField($modDetail, '[0]hargajualbhn', array('value'=>$model->hargajualbahan,'class'=>'integer'))
-                        .CHtml::activeHiddenField($modDetail, '[0]ukuran_bahanterima', array('value'=>$ukuran))
-                        .CHtml::activeHiddenField($modDetail, '[0]merk_bahanterima', array('value'=>$merk))
-                    .'</td>
-                    <td>'.CHtml::textField('noUrut',0,array('id'=>'noUrut','class'=>'noUrut span1', 'readonly'=>true)).'</td>
-                    <td>'.$model->golbahanmakanan->golbahanmakanan_nama.'</span></td>
-                    <td>'.$model->jenisbahanmakanan.'</td>
-                    <td>'.$model->kelbahanmakanan.'</td>
-                    <td>'.$model->namabahanmakanan.'</td>
-                    <td>'.$model->jmlpersediaan.'</td>
-                    <td>'.CHtml::activeDropDownList($modDetail, '[0]satuanbahan', LookupM::getItems('satuanbahanmakanan'), array( 'class'=>'span2 satuanbahan')).'</td>
-		    <td>'.CHtml::activeTextField($modDetail, '[0]harganettobahan', array('value'=>$model->harganettobahan, 'class'=>'span2 integer harganettobahan', 'onblur'=>'hitung(this);','readonly'=>false))
-			 .CHtml::activeHiddenField($modDetail, '[0]hargajualbahan', array('value'=>$model->hargajualbahan, 'class'=>'span2 integer hargajualbahan', 'readonly'=>true)).'</td>
-                    <td>'.CHtml::activeTextField($modDetail, '[0]discount', array('value'=>$model->discount, 'class'=>'discount span1 integer', 'onkeyup'=>'hitungTotalDiscount();')).'</td>
-                    <td><span name="[0][tglkadaluarsabahan]">'.MyFormatter::formatDateTimeForUser($model->tglkadaluarsabahan).'</span></td>
-                    
-                    <td>'.CHtml::activeTextField($modDetail, '[0]qty_terima', array('value'=>$qty, 'class'=>'span1 integer qty', 'onblur'=>'hitung(this);')).'</td>
-                    <td>'.CHtml::activeTextField($modDetail, '[0]subNetto', array('value'=>$subNetto, 'class'=>'span2 integer subNetto','readonly'=>true)).'</td>
-                    <td>'.CHtml::link("<span class='icon-form-silang'>&nbsp;</span>",'',array('href'=>'','onclick'=>'hapus(this);return false;','style'=>'text-decoration:none;', 'class'=>'cancel')).'</td>
-                    </tr>';
-            echo json_encode($tr);
+            $tr = $this->renderPartial('_rowMakanan', array(
+				'modDetail'=>$modDetail,
+				'model'=>$model,
+				'subNetto'=>$subNetto,
+				'qty'=>$qty,
+				'ukuran'=>$ukuran,
+				'merk'=>$merk,
+				'satuanbahan'=>$satuanbahan,
+			), true, true);
+            echo $tr;
             Yii::app()->end();
         }
     }
---
+++ protected/modules/gizi/views/terimabahanmakan/_jsFunctions.php
@@ -0,0 +1,180 @@
+<?php Yii::app()->clientScript->registerScriptFile(Yii::app()->baseUrl.'/js/accounting2.js', CClientScript::POS_END); ?>
+<?php Yii::app()->clientScript->registerScriptFile(Yii::app()->baseUrl.'/js/form2.js', CClientScript::POS_END); ?>
+
+<?php
+    $this->widget('application.extensions.moneymask.MMask', array(
+        'element' => '.numbersOnly',
+        'config' => array(
+            'defaultZero' => true,
+            'allowZero' => true,
+            'decimal' => ',',
+            'thousands' => '.',
+            'precision' => 0,
+        )
+    ));
+?>
+
+<script>
+function inputBahanMakanan(){
+		unformatNumberSemua();
+        var id = $('#idBahan').val();
+        var qty= $('#qty').val();
+        var ukuran = $('#ukuran').val();
+        var merk = $('#merk').val();
+        var satuanbahan = $('#satuanbahan').val();
+        if (jQuery.isNumeric(id)){
+            $.post('<?php echo $urlBahan; ?>',{id:id, qty:qty,ukuran:ukuran,merk:merk, satuanbahan:satuanbahan},function(data){
+                $('#tableBahanMakanan tbody').append(data);		
+		renameInputRowBahanMakanan('tableBahanMakanan');
+                hitungSemua();
+                hitungTotalDiscount();	    	
+                $("#tableBahanMakanan tbody tr:last .integer2").maskMoney({"defaultZero":true,"allowZero":true,"decimal":",","thousands":".","precision":0,"symbol":null});
+                $("#tableBahanMakanan tbody tr:last .satuanbahan").val(satuanbahan);
+				$("#tableBahanMakanan tbody tr:last .tanggal").datepicker(
+					jQuery.extend(
+						{showMonthAfterYear:true},
+						jQuery.datepicker.regional['id'],
+						{
+							'dateFormat':'dd M yy',
+							'changeYear':true,
+							'changeMonth':true,
+							'showAnim':'fold',
+							'yearRange':'-0y:+10y'
+						}
+					)
+				);
+				formatNumberSemua();
+            });
+        }
+        else{
+            myAlert('Isi Data dengan Benar');
+        }
+    }
+    
+    function hitungSemua(){
+		unformatNumberSemua();
+        value = 0;
+        $('.noUrut').each(function(){
+            $(this).parents('tr').find('#checkList').attr('name','checkList['+(noUrut-1)+']');
+//            $(this).val(noUrut);
+//            noUrut++;
+            if ($(this).parents('tr').find('#checkList').is(':checked')){
+                val = parseFloat($(this).parents('tr').find('.subNetto').val());
+                value += val;
+            }
+        });
+        hitungTotalDiscount();
+        $('#<?php echo $totalHarga; ?>').val(value);
+		formatNumberSemua();
+    }
+	   	
+    function hitung(obj){
+		//unformatNumberSemua();
+        var netto = parseFloat(unformatNumber($(obj).parents('tr').find('input[name$="[harganettobahan]"]').val()));
+        var jml = parseFloat(unformatNumber($(obj).parents('tr').find('input[name$="[qty_terima]"]').val()));
+		$(obj).parents('tr').find('.subNetto').val(formatNumber(netto*jml));
+		console.log(netto, jml);
+		hitungSemua();
+        hitungTotalDiscount();
+		//formatNumberSemua();
+    }
+    
+    function hapus(obj) {
+        $(obj).parents('tr').remove();
+        hitungSemua();
+    }
+    
+    function hitungTotal(obj){
+		unformatNumberSemua();
+        var netto = $('#TerimabahandetailT_harganettobhn').val();
+        var jml = $(obj).val();
+        $(obj).parents('tr').find('.subNetto').val(netto*jml);
+        hitungSemua();
+        hitungTotalDiscount();
+		formatNumberSemua();
+    }
+    
+    function hitungTotalDiscount(){
+		unformatNumberSemua();
+        var discountPersen = $('#discountpersen').val();
+        var totaldiscount = 0;
+            if (jQuery.isNumeric(discountPersen)){
+                $('.discount').each(function(){
+                    if ($(this).parents('tr').find('.cekList').is(':checked')){
+                        var subnetto = $(this).parents('tr').find('.subNetto').val();
+                        discount = subnetto*discountPersen/100;
+                        $(this).val(discount);
+                        totaldiscount+=discount;
+                    }
+                });
+            }
+            else{
+                $('.discount').each(function(){
+                    var discount = parseFloat($(this).val());
+                    if ($(this).parents('tr').find('#checkList').is(':checked')){
+                        totaldiscount+=discount;
+                    }
+                });      
+            }
+		formatNumberSemua();
+        $('#GZTerimabahanmakan_totaldiscount').val(totaldiscount);
+    }
+	
+	function renameInputRowBahanMakanan(obj_table){
+		var row = 0;
+		$('#'+obj_table).find("tbody > tr").each(function(){
+		$(this).find("#noUrut").val(row+1);
+		$(this).find('input,select,textarea').each(function(){ //element <input>
+			var old_name = $(this).attr("name").replace(/]/g,"");
+			var old_name_arr = old_name.split("[");
+			if(old_name_arr.length == 3){
+			$(this).attr("id",old_name_arr[0]+"_"+row+"_"+old_name_arr[2]);
+			$(this).attr("name",old_name_arr[0]+"["+row+"]["+old_name_arr[2]+"]");
+			}
+		});
+		row++;
+		});
+
+    }
+	
+	$(document).ready(function() {
+		renameInputRowBahanMakanan('tableBahanMakanan');
+		hitung();
+		setTimeout(function() {
+			$("#tableBahanMakanan tbody tr .tanggal").each(function() {
+				$(this).datepicker(
+					jQuery.extend(
+						{showMonthAfterYear:true},
+						jQuery.datepicker.regional['id'],
+						{
+							'dateFormat':'dd M yy',
+							'changeYear':true,
+							'changeMonth':true,
+							'showAnim':'fold',
+							'yearRange':'-0y:+10y'
+						}
+					)
+				);
+			});
+		}, 500);
+		
+		$("form").submit(function(){
+			supplier = $("#'.CHtml::activeId($model, 'supplier_id').'").val();
+			jumlah = 0;
+			if (!jQuery.isNumeric(supplier)){
+				myAlert("'.CHtml::encode($model->getAttributeLabel('supplier_id')).' harus diisi !");
+				return false;
+			}
+			$(".cekList").each(function(){
+				if ($(this).is(":checked")){
+					jumlah++;
+				}
+			});
+
+			if (jumlah < 1){
+				myAlert("Pilih Nama Bahan Makanan yang akan diajukan !");
+				return false;
+			}
+		});
+	});
+</script>
