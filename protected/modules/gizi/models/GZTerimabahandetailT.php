b380dcbf0e9e14aa6ff59ad26b69efa969fd4264
Iqbal Laksana <iqballaksana01@gmail.com>
2016-09-01 15:47:39 +0700

    ABE-1889: CLONE - ABE-1803 Tambah Menu Informasi Stok Bahan Makanan

--- protected/modules/gizi/models/GZTerimabahandetailT.php
+++ protected/modules/gizi/models/GZTerimabahandetailT.php
@@ -2,6 +2,8 @@
 class GZTerimabahandetailT extends TerimabahandetailT {
     
     public $subNetto;
+    public $tglterimabahan;
+    public $tgl;
 
     public static function model($className = __CLASS__) {
         return parent::model($className);
