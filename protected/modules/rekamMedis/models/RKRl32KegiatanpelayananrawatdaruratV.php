bad7017cae020c43b4352ec67f8e9a403a8cee3c
Iqbal Laksana <iqballaksana01@gmail.com>
2016-09-30 14:20:14 +0900

    ABE-2016: rawat inap + rawat darurat + gigi mulut + kebidanan

--- protected/modules/rekamMedis/models/RKRl32KegiatanpelayananrawatdaruratV.php
+++ protected/modules/rekamMedis/models/RKRl32KegiatanpelayananrawatdaruratV.php
@@ -2,6 +2,11 @@
 
 class RKRl32KegiatanpelayananrawatdaruratV extends Rl32KegiatanpelayananrawatdaruratV
 {
+        public $tgl_awal, $tgl_akhir;
+        public $bln_awal, $bln_akhir;
+        public $thn_awal, $thn_akhir;
+        public $jns_periode;
+        
 	/**
 	 * Returns the static model of the specified AR class.
 	 * @param string $className active record class name.
--- protected/modules/rekamMedis/models/RKRl33KegiatangigimulutV.php
+++ protected/modules/rekamMedis/models/RKRl33KegiatangigimulutV.php
@@ -2,6 +2,11 @@
 
 class RKRl33KegiatangigimulutV extends Rl33KegiatangigimulutV
 {
+        public $tgl_awal, $tgl_akhir;
+        public $bln_awal, $bln_akhir;
+        public $thn_awal, $thn_akhir;
+        public $jns_periode;
+        
 	/**
 	 * Returns the static model of the specified AR class.
 	 * @param string $className active record class name.
