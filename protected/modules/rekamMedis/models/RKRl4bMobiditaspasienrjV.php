69373cd35efe2b518cfd32a0802fa6057bc2de86
Iqbal Laksana <iqballaksana01@gmail.com>
2016-10-10 10:33:52 +0900

    ABE-2016: data keadaan morbiditas rawat inap + data keadaan morbiditas rawat jalan

--- protected/modules/rekamMedis/models/RKRl4bMobiditaspasienrjV.php
+++ protected/modules/rekamMedis/models/RKRl4bMobiditaspasienrjV.php
@@ -2,6 +2,10 @@
 
 class RKRl4bMobiditaspasienrjV extends Rl4bMobiditaspasienrjV
 {
+        public $tgl_awal, $tgl_akhir;
+        public $bln_awal, $bln_akhir;
+        public $thn_awal, $thn_akhir;
+        public $jns_periode;
 
 	public static function model($className=__CLASS__)
 	{
