958a849988732465eb1d648d3d463234eaf4debb
Iqbal Laksana <iqballaksana01@gmail.com>
2016-11-25 08:05:08 +0900

    ABE-2023: informasi umur piutang part 1

--- protected/modules/akuntansi/controllers/InformasiUmurPiutangController.php
+++ protected/modules/akuntansi/controllers/InformasiUmurPiutangController.php
@@ -1,7 +1,24 @@
 <?php
 
-Yii::import("manajemenAset.controllers.InformasiPenjualanController");
-Yii::import("manajemenAset.models.*");
-class InformasiPenjualanAKController extends InformasiPenjualanController {
+class InformasiUmurPiutangController extends MyAuthController {
+        
+    public $layout = '//layouts/column1';
+    public $path_view = 'akuntansi.views.informasiUmurPiutang.';
     
+    public function actionIndex()
+    {
+        $model = new MAInvtanahT;
+        $model->tgl_awal = date('Y-m-d');
+        $model->tgl_akhir = date('Y-m-d');
+        
+        if (isset($_GET['MAInvtanahT'])){
+            $model->attributes = $_GET['MAInvtanahT'];
+            $model->barang_nama = $_GET['MAInvtanahT']['barang_nama'];
+            $model->tgl_awal = MyFormatter::formatDateTimeForDb($_GET['MAInvtanahT']['tgl_awal']);
+            $model->tgl_akhir = MyFormatter::formatDateTimeForDb($_GET['MAInvtanahT']['tgl_akhir']);
+        }
+        
+        $this->render($this->path_view.'tanah/index', array('model'=>$model));
+        
+    }
 }
