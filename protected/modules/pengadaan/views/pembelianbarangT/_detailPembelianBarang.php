abbf52c5393f602bb06fa7b5aa55aecec1696b3e
Iqbal Laksana <iqballaksana01@gmail.com>
2017-02-02 09:54:36 +0700

    ABE-2306: fix pada tabel permintaan pembelian barang kolom harga satuan dan harga beli ditambahkan (Rp) + fix pada tabel permintaan pembelian barang kolom isi kemasan dan jumlah permintaan hanya dapat diisi numeric saja

--- protected/modules/pengadaan/views/pembelianbarangT/_detailPembelianBarang.php
+++ protected/modules/pengadaan/views/pembelianbarangT/_detailPembelianBarang.php
@@ -18,8 +18,8 @@
     <td><?php echo $modBarang->barang_merk; ?></td>        
     <td><?php echo $modBarang->barang_ukuran; ?></td>
     <td><?php echo $modBarang->barang_ekonomis_thn; ?></td>
-    <td><?php echo CHtml::activeTextField($modDetail, '[]jmldlmkemasan', array('class'=>'span1 numbersOnly', 'style'=>'text-align: right;')); ?></td>    
-    <td><?php echo CHtml::activeTextField($modDetail, '[]jmlbeli', array('class'=>'span1 numbersOnly qty', 'style'=>'text-align: right;', 'onblur'=>'hitungTotal(this)')).' '.$modBarang->barang_satuan; ?></td>
+    <td><?php echo CHtml::activeTextField($modDetail, '[]jmldlmkemasan', array('class'=>'span1 integer2', 'style'=>'text-align: right;')); ?></td>    
+    <td><?php echo CHtml::activeTextField($modDetail, '[]jmlbeli', array('class'=>'span1 integer2 qty', 'style'=>'text-align: right;', 'onblur'=>'hitungTotal(this)')).' '.$modBarang->barang_satuan; ?></td>
     <td><?php echo CHtml::activeTextField($modDetail, '[]hargasatuan', array('class'=>'span2 integer2 satuan', 'style'=>'text-align: right;', 'onblur'=>'hitungTotal(this)')); ?></td>
     <td><?php echo CHtml::activeTextField($modDetail, '[]hargabeli', array('class'=>'span2 integer2 beli', 'style'=>'text-align: right;', 'onblur'=>true)); ?></td>
     <!--<td><?php //echo CHtml::activeDropDownList($modDetail, '[]satuanbeli', LookupM::getItems('satuanbarang'), array('empty'=>'-- Pilih --', 'class'=>'span2')); ?></td>    -->
