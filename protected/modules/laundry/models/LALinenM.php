d0588c4a1a8ee19aa7957acef7e391af9089b261
Iqbal Laksana <iqbal.laksana@piindonesia.co.id>
2017-05-08 17:14:56 +0700

    ABE-2336: fix transaksi pengajuan perawatan linen

--- protected/modules/laundry/models/LALinenM.php
+++ protected/modules/laundry/models/LALinenM.php
@@ -1,5 +1,9 @@
 <?php
 class LALinenM extends LinenM {
+	
+	public $barang_nama;
+	public $jenislinen_nama;
+	
 	public static function model($className=__CLASS__)
 	{
 		return parent::model($className);
@@ -10,54 +14,57 @@ class LALinenM extends LinenM {
 	   // Warning: Please modify the following code to remove attributes that
 	   // should not be searched.
 
-	   $criteria=new CDbCriteria;
-            
+	    $criteria=new CDbCriteria;
+        $criteria->join = " LEFT JOIN  barang_m b ON b.barang_id = t.barang_id  "
+						. " LEFT JOIN  jenislinen_m jl ON jl.jenislinen_id = t.jenislinen_id   "  ;
 		if(!empty($this->linen_id)){
-			$criteria->addCondition('linen_id = '.$this->linen_id);
+			$criteria->addCondition('t.linen_id = '.$this->linen_id);
 		}
 		if(!empty($this->jenislinen_id)){
-			$criteria->addCondition('jenislinen_id = '.$this->jenislinen_id);
+			$criteria->addCondition('t.jenislinen_id = '.$this->jenislinen_id);
 		}
 		if(!empty($this->ruangan_id)){
-			$criteria->addCondition('ruangan_id = '.$this->ruangan_id);
+			$criteria->addCondition('t.ruangan_id = '.$this->ruangan_id);
 		}
 		if(!empty($this->rakpenyimpanan_id)){
-			$criteria->addCondition('rakpenyimpanan_id = '.$this->rakpenyimpanan_id);
+			$criteria->addCondition('t.rakpenyimpanan_id = '.$this->rakpenyimpanan_id);
 		}
 		if(!empty($this->bahanlinen_id)){
-			$criteria->addCondition('bahanlinen_id = '.$this->bahanlinen_id);
+			$criteria->addCondition('t.bahanlinen_id = '.$this->bahanlinen_id);
 		}
 		if(!empty($this->barang_id)){
-			$criteria->addCondition('barang_id = '.$this->barang_id);
+			$criteria->addCondition('t.barang_id = '.$this->barang_id);
 		}
-		$criteria->compare('LOWER(kodelinen)',strtolower($this->kodelinen),true);
-		$criteria->compare('LOWER(tglregisterlinen)',strtolower($this->tglregisterlinen),true);
-		$criteria->compare('LOWER(noregisterlinen)',strtolower($this->noregisterlinen),true);
-		$criteria->compare('LOWER(namalinen)',strtolower($this->namalinen),true);
-		$criteria->compare('LOWER(namalainnya)',strtolower($this->namalainnya),true);
-		$criteria->compare('LOWER(merklinen)',strtolower($this->merklinen),true);
+		$criteria->compare('LOWER(t.kodelinen)',strtolower($this->kodelinen),true);
+		$criteria->compare('LOWER(t.tglregisterlinen)',strtolower($this->tglregisterlinen),true);
+		$criteria->compare('LOWER(t.noregisterlinen)',strtolower($this->noregisterlinen),true);
+		$criteria->compare('LOWER(t.namalinen)',strtolower($this->namalinen),true);
+		$criteria->compare('LOWER(t.namalainnya)',strtolower($this->namalainnya),true);
+		$criteria->compare('LOWER(t.merklinen)',strtolower($this->merklinen),true);
 		if(!empty($this->beratlinen)){
-			$criteria->addCondition('beratlinen = '.$this->beratlinen);
+			$criteria->addCondition('t.beratlinen = '.$this->beratlinen);
 		}
-		$criteria->compare('LOWER(warna)',strtolower($this->warna),true);
-		$criteria->compare('LOWER(tahunbeli)',strtolower($this->tahunbeli),true);
-		$criteria->compare('LOWER(gambarlinen)',strtolower($this->gambarlinen),true);
+		$criteria->compare('LOWER(t.warna)',strtolower($this->warna),true);
+		$criteria->compare('LOWER(t.tahunbeli)',strtolower($this->tahunbeli),true);
+		$criteria->compare('LOWER(t.gambarlinen)',strtolower($this->gambarlinen),true);
 		if(!empty($this->jmlcucilinen)){
-			$criteria->addCondition('jmlcucilinen = '.$this->jmlcucilinen);
+			$criteria->addCondition('t.jmlcucilinen = '.$this->jmlcucilinen);
 		}
-		$criteria->compare('LOWER(create_time)',strtolower($this->create_time),true);
-		$criteria->compare('LOWER(update_time)',strtolower($this->update_time),true);
+		$criteria->compare('LOWER(t.create_time)',strtolower($this->create_time),true);
+		$criteria->compare('LOWER(t.update_time)',strtolower($this->update_time),true);
 		if(!empty($this->create_loginpemakai_id)){
-			$criteria->addCondition('create_loginpemakai_id = '.$this->create_loginpemakai_id);
+			$criteria->addCondition('t.create_loginpemakai_id = '.$this->create_loginpemakai_id);
 		}
 		if(!empty($this->update_loginpemakai_id)){
-			$criteria->addCondition('update_loginpemakai_id = '.$this->update_loginpemakai_id);
+			$criteria->addCondition('t.update_loginpemakai_id = '.$this->update_loginpemakai_id);
 		}
 		if(!empty($this->create_ruangan)){
-			$criteria->addCondition('create_ruangan = '.$this->create_ruangan);
+			$criteria->addCondition('t.create_ruangan = '.$this->create_ruangan);
 		} 
-		$criteria->compare('linen_aktif',$this->linen_aktif);
-		$criteria->compare('LOWER(satuanlinen)',strtolower($this->satuanlinen),true);
+		$criteria->compare('t.linen_aktif',$this->linen_aktif);
+		$criteria->compare('LOWER(t.satuanlinen)',strtolower($this->satuanlinen),true);
+		$criteria->compare('LOWER(b.barang_nama)',strtolower($this->barang_nama),true);
+		$criteria->compare('LOWER(jl.jenislinen_nama)',strtolower($this->jenislinen_nama),true);
 		// $criteria->addCondition("linen_id in(select linen_id from penyimpananlinen_t)");
 		// $criteria->limit=10;
              
